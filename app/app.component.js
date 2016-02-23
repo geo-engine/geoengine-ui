38452d8d33dc210ceaef138c0e6e0e59b933952a
Nikolaus Glombiewski <glombien@mathematik.uni-marburg.de>
2016-02-23 17:02:35 +0100

    added basic toolbar example

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1) {
+System.register(['angular2/core', 'ng2-material/all'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,31 +8,28 @@ System.register(['angular2/core'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, all_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (all_1_1) {
+                all_1 = all_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
-                    this.title = "Hello World!";
-                    this.people = [
-                        { "name": "Bob" },
-                        { "name": "Tim" },
-                        { "name": "Mike" }
-                    ];
-                    this.tabNr = 1;
                 }
-                AppComponent.prototype.traverse = function () {
-                    this.tabNr = (this.tabNr + 1) % 3;
+                AppComponent.prototype.clicked = function (message) {
+                    alert(message);
                 };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'wave-app',
-                        template: "\n    <div layout=\"column\" style=\"height:500px;\">\n\t\t\t\t<md-toolbar>\n\t\t\t\t  <paper-icon-button icon=\"menu\"></paper-icon-button>\n\t\t\t\t  <div class=\"title\">{{title}}</div>\n\t\t\t\t  <paper-icon-button icon=\"more-vert\"></paper-icon-button>\n\t\t\t\t</md-toolbar>\n\t\t\t\t<paper-toolbar>\n\t\t\t\t\t<paper-tabs [selected]=\"tabNr\" (selected-changed)=\"tabNr=$event.detail.value\">\n\t\t\t\t\t\t<paper-tab>Tab 1</paper-tab>\n\t\t\t\t\t\t<paper-tab>Tab 2</paper-tab>\n\t\t\t\t\t\t<paper-tab>Tab 3</paper-tab>\n\t\t\t\t\t</paper-tabs>\n\t\t\t\t\t<br />\n\t\t\t\t\t<iron-pages [selected]=\"tabNr\">\n\t\t\t\t\t\t<div>Page 1</div>\n\t\t\t\t\t\t<div>Page 2</div>\n\t\t\t\t\t\t<div>Page 3</div>\n\t\t\t\t\t</iron-pages>\n\t\t\t\t\t<br />\n\t\t\t\t\t<div (click)=\"traverse()\">Page {{tabNr + 1}}</div>\n\t\t\t\t</paper-toolbar>\n    </div>\n    "
+                        templateUrl: 'templates/app.component.html',
+                        directives: [all_1.MATERIAL_DIRECTIVES]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
@@ -42,11 +39,4 @@ System.register(['angular2/core'], function(exports_1) {
         }
     }
 });
-/*
-                <iron-list [items]="people" as="item">
-                    <template>
-                        <div>Name: [[item.name]]</div>
-                    </template>
-                </iron-list>
-*/ 
 //# sourceMappingURL=app.component.js.map
--- src/app.component.ts
+++ src/app.component.ts
@@ -1,53 +1,13 @@
 import {Component} from 'angular2/core';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 
 @Component({
     selector: 'wave-app',
-    template: `
-    <div layout="column" style="height:500px;">
-				<md-toolbar>
-				  <paper-icon-button icon="menu"></paper-icon-button>
-				  <div class="title">{{title}}</div>
-				  <paper-icon-button icon="more-vert"></paper-icon-button>
-				</md-toolbar>
-				<paper-toolbar>
-					<paper-tabs [selected]="tabNr" (selected-changed)="tabNr=$event.detail.value">
-						<paper-tab>Tab 1</paper-tab>
-						<paper-tab>Tab 2</paper-tab>
-						<paper-tab>Tab 3</paper-tab>
-					</paper-tabs>
-					<br />
-					<iron-pages [selected]="tabNr">
-						<div>Page 1</div>
-						<div>Page 2</div>
-						<div>Page 3</div>
-					</iron-pages>
-					<br />
-					<div (click)="traverse()">Page {{tabNr + 1}}</div>
-				</paper-toolbar>
-    </div>
-    `
+    templateUrl: 'templates/app.component.html',
+	directives: [MATERIAL_DIRECTIVES]
 })
 export class AppComponent {
-	title = "Hello World!";
-	
-	people = [
-	  {"name": "Bob"},
-	  {"name": "Tim"},
-	  {"name": "Mike"}
-	];
-	
-	tabNr = 1;
-	
-	traverse() {
-		this.tabNr = (this.tabNr + 1) % 3;
-	}
-	
+    clicked(message: string) {
+        alert(message);
+    }
 }
-
-/*
-				<iron-list [items]="people" as="item">
-					<template>
-						<div>Name: [[item.name]]</div>
-					</template>
-				</iron-list>
-*/
