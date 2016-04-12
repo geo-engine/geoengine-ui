ebcf19395a898c00490f19f89d190fefcfb1f153
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2016-04-12 17:13:49 +0200

    Extracted a `StorageProvider` from the `StorageService`.
    This allows adding a REST storage more easily later on.

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -9,145 +9,271 @@ import {Project} from "../models/project.model";
 import {Operator, ResultType} from "../models/operator.model";
 
 interface LayerSerialization {
-  operator: string;
-  expanded: boolean;
+    operator: string;
+    expanded: boolean;
 }
 
+/**
+ * This service allows persisting the current execution context.
+ */
 @Injectable()
 export class StorageService {
-  constructor(private layerService: LayerService, private projectService: ProjectService) {
-    this.loadProject();
-    this.loadLayers();
-    this.storeProjectSetup();
-    this.storeLayersSetup();
-  }
+    private storageProvider: StorageProvider;
 
-  private loadLayers() {
-    let layersJSON = localStorage.getItem("layers");
-    if (layersJSON === null) {
-      // default
-      this.layerService.setLayers([
-          new Layer(new Operator(
-              "source",
-              ResultType.RASTER,
-              new Map<string, string | number>().set("channel", 0)
-                                                .set("sourcename", "srtm"),
-              "EPSG:4326",
-              "SRTM"
-          )),
-          new Layer(new Operator(
-              "gfbiopointsource",
-              ResultType.POINTS,
-              new Map<string, string | number>()
-                  .set("datasource", "GBIF")
-                  .set("query", `{"globalAttributes":{"speciesName":"Puma concolor"},"localAttributes":{}}`),
-              "EPSG:4326",
-              "Puma Concolor"
-          ))
-      ]);
+    constructor(private layerService: LayerService, private projectService: ProjectService) {
+        this.storageProvider = new BrowserStorageProvider();
 
-    } else {
-      // load
-      let layers: Array<Layer> = [];
-      let layerDicts: Array<LayerSerialization> = JSON.parse(layersJSON);
-
-      for (let layerDict of layerDicts) {
-        let layer = new Layer(Operator.fromJSON(layerDict.operator));
-        layer.expanded = layerDict.expanded;
-
-        layers.push(layer);
-      }
-
-      // console.log(layers);
-
-      this.layerService.setLayers(layers);
+        this.loadProject();
+        this.loadLayers();
+        this.storeProjectSetup();
+        this.storeLayersSetup();
     }
-  }
 
-  private storeLayersSetup() {
-    this.layerService.getLayers().subscribe((layers: Array<Layer>) => {
-      let layerStrings: Array<LayerSerialization> = [];
+    private loadLayers() {
+        let layers = this.storageProvider.loadLayers();
 
-      for (let layer of layers) {
-        layerStrings.push({
-          operator: layer.operator.toJSON(),
-          expanded: layer.expanded
-        });
-      }
-
-      // console.log("store", "layers", layerStrings);
-      localStorage.setItem("layers", JSON.stringify(layerStrings));
-    });
-  }
-
-  private loadProject() {
-      let projectJSON = localStorage.getItem("project");
-      if (projectJSON === null) {
-          // use default project
-      } else {
-          let project = Project.fromJSON(projectJSON);
-          this.projectService.setProject(project);
-      }
-  }
-
-  private storeProjectSetup() {
-    this.projectService.getProject().subscribe((project: Project) => {
-        localStorage.setItem("project", project.toJSON());
-    });
-  }
-
-  addLayerListVisibleObservable(layerListVisible$: Observable<boolean>) {
-    layerListVisible$.subscribe(visible => {
-      localStorage.setItem("layerListVisible", JSON.stringify(visible));
-      // console.log("store", "layerListVisible$", visible);
-    });
-  }
-
-  getLayerListVisible(): boolean {
-    let layerListVisible = localStorage.getItem("layerListVisible");
-    // console.log("load", "layerListVisible$", layerListVisible);
-    if (layerListVisible === null) {
-      // default
-      return true;
-    } else {
-      // load and parse
-      return JSON.parse(layerListVisible);
+        if (layers === undefined) {
+            // default
+            // TODO: should be empty later on
+            this.layerService.setLayers([
+                new Layer(new Operator(
+                    "source",
+                    ResultType.RASTER,
+                    new Map<string, string | number>().set("channel", 0)
+                    .set("sourcename", "srtm"),
+                    "EPSG:4326",
+                    "SRTM"
+                )),
+                new Layer(new Operator(
+                    "gfbiopointsource",
+                    ResultType.POINTS,
+                    new Map<string, string | number>()
+                    .set("datasource", "GBIF")
+                    .set("query", `{"globalAttributes":{"speciesName":"Puma concolor"},"localAttributes":{}}`),
+                    "EPSG:4326",
+                    "Puma Concolor"
+                ))
+            ]);
+        } else {
+            this.layerService.setLayers(layers);
+        }
     }
-  }
 
-  addDataTableVisibleObservable(dataTableVisible$: Observable<boolean>) {
-    dataTableVisible$.subscribe(visible => {
-      localStorage.setItem("dataTableVisible", JSON.stringify(visible));
-    });
-  }
-
-  getDataTableVisible(): boolean {
-    let dataTableVisible = localStorage.getItem("dataTableVisible");
-    if (dataTableVisible === null) {
-      // default
-      return true;
-    } else {
-      // load and parse
-      return JSON.parse(dataTableVisible);
+    private storeLayersSetup() {
+        this.layerService.getLayers().subscribe(this.storageProvider.saveLayers);
     }
-  }
 
-  addTabIndexObservable(tabIndex$: Observable<number>) {
-    tabIndex$.subscribe(index => {
-        // console.log("tindex", index);
-        localStorage.setItem("tabIndex", JSON.stringify(index));
-    });
-  }
-
-  getTabIndex(): number {
-    let tabIndex = localStorage.getItem("tabIndex");
-    // console.log("tindex", tabIndex);
-    if (tabIndex === null) {
-      // default
-      return 0;
-    } else {
-      // load and parse
-      return JSON.parse(tabIndex);
+    private loadProject() {
+        let project = this.storageProvider.loadProject();
+        if (project === undefined) {
+            // use default project
+        } else {
+            this.projectService.setProject(project);
+        }
     }
-  }
+
+    private storeProjectSetup() {
+        this.projectService.getProject().subscribe(this.storageProvider.saveProject);
+    }
+
+    addLayerListVisibleObservable(layerListVisible$: Observable<boolean>) {
+        layerListVisible$.subscribe(this.storageProvider.saveLayerListVisible);
+    }
+
+    getLayerListVisible(): boolean {
+        let layerListVisible = this.storageProvider.loadLayerListVisible();
+        if (layerListVisible === undefined) {
+            // default
+            return true;
+        } else {
+            // load and parse
+            return layerListVisible;
+        }
+    }
+
+    addDataTableVisibleObservable(dataTableVisible$: Observable<boolean>) {
+        dataTableVisible$.subscribe(this.storageProvider.saveDataTableVisible);
+    }
+
+    getDataTableVisible(): boolean {
+        let dataTableVisible = this.storageProvider.loadDataTableVisible();
+        if (dataTableVisible === undefined) {
+            // default
+            return true;
+        } else {
+            // load and parse
+            return dataTableVisible;
+        }
+    }
+
+    addTabIndexObservable(tabIndex$: Observable<number>) {
+        tabIndex$.subscribe(this.storageProvider.saveTabIndex);
+    }
+
+    getTabIndex(): number {
+        let tabIndex = this.storageProvider.loadTabIndex();
+        if (tabIndex === undefined) {
+            // default
+            return 0;
+        } else {
+            // load and parse
+            return tabIndex;
+        }
+    }
+}
+
+/**
+ * Storage Provider that allows saving and retrieval of WAVE settings and items.
+ */
+interface StorageProvider {
+
+    /**
+     * Load the current project.
+     * @returns A project instance.
+     */
+    loadProject(): Project;
+
+    /**
+     * Save the current project.
+     * @param project A project instance.
+     */
+    saveProject(project: Project): void;
+
+    /**
+     * Load the current layers.
+     * @returns An array of layers.
+     */
+    loadLayers(): Array<Layer>;
+
+    /**
+     * Save the current layers.
+     * @param layers An array if layers.
+     */
+    saveLayers(layers: Array<Layer>): void;
+
+    /**
+     * Load the current layer list visibility.
+     * @returns The visibility.
+     */
+    loadLayerListVisible(): boolean;
+
+    /**
+     * Save the current layer list visiblity.
+     * @param visible The visibility.
+     */
+    saveLayerListVisible(visible: boolean): void;
+
+    /**
+     * Load the current data table visibility.
+     * @returns The visibility.
+     */
+    loadDataTableVisible(): boolean;
+
+    /**
+     * Save the current data table visiblity.
+     * @param visible The visibility.
+     */
+    saveDataTableVisible(visible: boolean): void;
+
+    /**
+     * Load the current tab index of the tob component.
+     * @returns The tab index.
+     */
+    loadTabIndex(): number;
+
+    /**
+     * Save the current tab index of the tab component.
+     * @param tabIndex The tab index.
+     */
+    saveTabIndex(tabIndex: number): void;
+}
+
+/**
+ * StorageProvider implementation that uses the brower's localStorage
+ */
+class BrowserStorageProvider implements StorageProvider {
+    loadProject(): Project {
+        let projectJSON = localStorage.getItem("project");
+        if (projectJSON === null) {
+            return undefined;
+        } else {
+            let project = Project.fromJSON(projectJSON);
+            return project;
+        }
+    }
+
+    saveProject(project: Project) {
+      localStorage.setItem("project", project.toJSON());
+    }
+
+    loadLayers(): Array<Layer> {
+        let layersJSON = localStorage.getItem("layers");
+        if (layersJSON === null) {
+            return undefined;
+        } else {
+            let layers: Array<Layer> = [];
+            let layerDicts: Array<LayerSerialization> = JSON.parse(layersJSON);
+
+            for (let layerDict of layerDicts) {
+                let layer = new Layer(Operator.fromJSON(layerDict.operator));
+                layer.expanded = layerDict.expanded;
+
+                layers.push(layer);
+            }
+
+            return layers;
+        }
+    }
+
+    saveLayers(layers: Array<Layer>) {
+        let layerStrings: Array<LayerSerialization> = [];
+
+        for (let layer of layers) {
+          layerStrings.push({
+            operator: layer.operator.toJSON(),
+            expanded: layer.expanded
+          });
+        }
+
+        localStorage.setItem("layers", JSON.stringify(layerStrings));
+    }
+
+    loadLayerListVisible(): boolean {
+        let layerListVisible = localStorage.getItem("layerListVisible");
+        if (layerListVisible === null) {
+            return undefined;
+        } else {
+            return JSON.parse(layerListVisible);
+        }
+    }
+
+    saveLayerListVisible(visible: boolean) {
+        localStorage.setItem("layerListVisible", JSON.stringify(visible));
+    }
+
+    loadDataTableVisible(): boolean {
+        let dataTableVisible = localStorage.getItem("dataTableVisible");
+        if (dataTableVisible === null) {
+            return undefined;
+        } else {
+            return JSON.parse(dataTableVisible);
+        }
+    }
+
+    saveDataTableVisible(visible: boolean) {
+        localStorage.setItem("dataTableVisible", JSON.stringify(visible));
+    }
+
+    loadTabIndex(): number {
+        let tabIndex = localStorage.getItem("tabIndex");
+        if (tabIndex === null) {
+            return undefined;
+        } else {
+            return JSON.parse(tabIndex);
+        }
+    }
+
+    saveTabIndex(tabIndex: number) {
+        localStorage.setItem("tabIndex", JSON.stringify(tabIndex));
+    }
 }
