44e282137c3f8d7ad82a369fa6f2ba51565b19da
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2016-07-25 14:44:27 +0200

    changed WAVE to VAT logo
    T88

--- src/components/info-area.component.ts
+++ src/components/info-area.component.ts
@@ -14,6 +14,59 @@ import {LoginDialogComponent} from '../users/login-dialog.component';
 import {UserService} from '../users/user.service';
 import {LayoutService} from '../app/layout.service';
 
+@Component({
+    selector: 'wave-vat-logo',
+    template: `
+    <h1><span class="blue">V</span
+        ><span class="light-blue">A</span
+        ><span class="green">T</span
+    ></h1>
+    `,
+    styles: [`
+    h1 {
+        background: #fff;
+        height: 3rem;
+        padding: 0.5rem;
+        border-radius: 3px;
+        margin: calc((82px - 4rem) / 2) auto;
+    }
+    .blue {
+        color: #3258a1;
+    }
+    .light-blue {
+        color: #3cace4;
+    }
+    .green {
+        color: #8cb74c;
+    }
+    `],
+})
+class VatLogoComponent {}
+
+@Component({
+    selector: 'wave-idessa-logo',
+    template: `
+    <h1>
+        <img src="assets/logo_idessa.png">
+        <span>IDESSA</span>
+    </h1>
+    `,
+    styles: [`
+    h1 {
+        opacity: 1;
+        font-size: 34px;
+    }
+    img {
+        vertical-align: middle;
+        height: 41px;
+    }
+    span {
+        opacity: 0.5;
+    }
+    `],
+})
+class IdessaLogoComponent {}
+
 /**
  * The top left info area component for user info and layer list collapsing.
  */
@@ -31,17 +84,8 @@ import {LayoutService} from '../app/layout.service';
         </button>
         <md-toolbar-row class="title-bar">
             <md-divider></md-divider>
-            ${Config.PROJECT === 'GFBio' ? `
-                <h1>WAVE</h1>
-            ` : `
-                <h1 style="width: calc(100% - 2*14px); opacity: 1;">
-                    <img
-                        src="assets/logo_idessa.png"
-                        style="vertical-align: middle; height: 41px;"
-                    >
-                    <span style="opacity: 0.5;">IDESSA</span>
-                </h1>
-            `}
+            <wave-vat-logo *ngIf="${Config.PROJECT === 'GFBio'}"></wave-vat-logo>
+            <wave-idessa-logo *ngIf="${Config.PROJECT === 'IDESSA'}"></wave-idessa-logo>
             <md-divider></md-divider>
         </md-toolbar-row>
         <md-toolbar-row>
@@ -73,13 +117,15 @@ import {LayoutService} from '../app/layout.service';
     md-toolbar >>> md-toolbar-row {
         height: 48px;
     }
-    md-toolbar {
-        ${Config.PROJECT === 'IDESSA' ? 'background: white !important;' : ''}
-        ${Config.PROJECT === 'IDESSA' ? 'color: black !important;' : ''}
-    }
-    md-toolbar >>> button {
-        ${Config.PROJECT === 'IDESSA' ? 'color: black !important;' : ''}
-    }
+    ${Config.PROJECT === 'IDESSA' ? `
+        md-toolbar {
+            background: white !important;
+            color: black !important;
+        }
+        md-toolbar >>> button {
+            color: black !important;
+        }
+    ` : ''}
     .title-bar {
         height: calc(100% - 96px);
         flex-direction: column;
@@ -105,7 +151,10 @@ import {LayoutService} from '../app/layout.service';
         background-color: transparent;
     }
     `],
-    directives: [CORE_DIRECTIVES, MATERIAL_DIRECTIVES, MdToolbar, DialogLoaderComponent],
+    directives: [
+        CORE_DIRECTIVES, MATERIAL_DIRECTIVES, MdToolbar, DialogLoaderComponent, VatLogoComponent,
+        IdessaLogoComponent,
+    ],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InfoAreaComponent {
