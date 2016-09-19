c4e2286695eb79d034071893f0629f5aa6f35e46
Johannes Drönner <droenner@Mathematik.Uni-Marburg.DE>
2016-09-19 17:52:48 +0200

    remove local (line) id

--- src/components/data-table.component.ts
+++ src/components/data-table.component.ts
@@ -226,9 +226,9 @@ export class DataTableComponent implements OnInit, OnChanges, AfterViewInit, OnD
             this.rows = [];
 
             if (features.length > 0) {
-                if ( features[0].id ) {
-                    this.columns = [{name: 'id', type: 'string', unit: undefined}];
-                };
+                //if ( features[0].id ) {
+                //    this.columns = [{name: 'id', type: 'string', unit: undefined}];
+                //};
                 if (features[0].properties) {
                     this.propertyColumns = Object.keys(features[0].properties).filter(x => !x.startsWith('___'))
                         .map(x => { return {name: x, type: '', unit: ''} as Column; });
