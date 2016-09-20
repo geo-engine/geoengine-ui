6d7a45a64d4084858abc842a095b7d193166c0a8
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2016-09-20 15:44:07 +0200

    fixed color wheel

--- src/services/random-color.service.ts
+++ src/services/random-color.service.ts
@@ -10,11 +10,11 @@ export class RandomColorService {
         [0, 255, 255], // '#00FFFF', // cyan
         [255, 0, 255], // '#FF00FF', // pink
         [255, 128, 0], // '#FF8000', // orange
-        [255, 128, 0], // '#FF0080', // strong pink
-        [255, 128, 0], // '#80FF00', // light green
-        [255, 128, 0], // '#00FF80', // blue green
-        [255, 128, 0], // '#0080FF', // light blue
-        [255, 128, 0], // '#8000FF', // purple
+        [255, 0, 128], // '#FF0080', // strong pink
+        [128, 255, 0], // '#80FF00', // light green
+        [0, 255, 128], // '#00FF80', // blue green
+        [0, 128, 255], // '#0080FF', // light blue
+        [128, 0, 255], // '#8000FF', // purple
     ];
     private colorIndex: number = 0;
 
