1412466e8c0148e4717323a1e58da3415bdbe8ba
Julian Maerte <jmaerte@mathematik.uni-marburg.de>
2017-03-09 14:54:40 +0100

    Simplified file uploader. Added more information to last page. TODO: Limit the header row counter TODO: Initially select text for typings drop  down.

--- src/app/operators/dialogs/csv/file-upload/file-upload.component.ts
+++ src/app/operators/dialogs/csv/file-upload/file-upload.component.ts
@@ -1,114 +1,107 @@
 /**
  * Created by Julian on 24.02.2017.
  */
-import {Component, ElementRef} from '@angular/core';
+import {Component} from '@angular/core';
 import {CSV} from '../csv-config/csv-config.component';
-import {Input} from '@angular/core/src/metadata/directives';
+import {BehaviorSubject, Observable} from "rxjs";
+
+enum FormStatus {
+    Selection,
+    Loading,
+    Finished
+}
 
 @Component({
-  selector: 'wave-csv-upload',
-  templateUrl: 'file-upload-template.component.html',
-  styleUrls: ['file-upload-style.component.css']
+    selector: 'wave-csv-upload',
+    templateUrl: 'file-upload-template.component.html',
+    styleUrls: ['file-upload-style.component.css']
 })
 export class CsvUploadComponent {
 
-  data: {
-      file: File,
-      content: string,
-      progress: number,
-      configured: boolean,
-      isNull: boolean
-  } = {
-      file: null,
-      content: '',
-      progress: 0,
-      configured: false,
-      isNull: true
-  };
+    status$: BehaviorSubject<FormStatus> = new BehaviorSubject<FormStatus>(FormStatus.Selection);
+    isSelecting$: Observable<boolean>;
+    isLoading$: Observable<boolean>;
+    isFinished$: Observable<boolean>;
 
-  result: CSV;
-  editing: boolean;
-
-  changeListener($event): void {
-    if ($event.target.files.length > 0) {
-      this.data = {
-          file: $event.target.files[0],
-          content: '',
-          progress: 0,
-          configured: false,
-          isNull: false
-      };
-      $event.target.value = '';
-    } else {
-        this.data = {
-            file: null,
-            content: '',
-            progress: 0,
-            configured: false,
-            isNull: true
-        };
-    }
-  }
-
-  upload() {
-    let reader: FileReader = new FileReader();
-
-    reader.onload = ((e) => {
-      this.data.content = reader.result;
-      this.data.progress = 100;
-      this.configure();
-    });
-    reader.onerror = ((e) => {
-      console.log('Error encountered file upload');
-    });
-    reader.onprogress = ((e) => {
-      this.data.progress = e.loaded / e.total * 100;
-      console.log('progressed: ' + e.loaded / e.total * 100 + '% on file');
-    });
-    reader.onloadstart = ((e) => {
-      console.log('Reader started reading ' + this.data.file.name);
-    });
-
-    reader.readAsText(this.data.file);
-  }
-
-  configure() {
-    if (this.data.isNull) {
-        return;
-    }
-    this.editing = true;
-  }
-
-  delete() {
-    this.editing = false;
-    this.data = {
+    data: {
+        file: File,
+        content: string,
+        progress: number,
+        isNull: boolean
+    } = {
         file: null,
         content: '',
         progress: 0,
-        configured: false,
         isNull: true
     };
-    this.result = null;
-  }
 
-  submit(e:CSV) {
-      console.log(e);
-      this.result = e;
-      this.data.configured = true;
-      this.editing = false;
-  }
+    result: CSV;
 
-  /**This method generates an number array containing all integers i with n <= i < m
-   *
-   * @param n lowest integer. !Warning: This integer is still contained in array.
-   * @param m highest integer. !Warning: This integer is not contained in array.
-   * @returns {number[]} Array {n,..,m-1}
-   */
-  range(n: number, m: number): number[] {
-    let res: number[] = [];
-    for (let i: number = n; i < m; i++) {
-      res.push(i);
+    constructor(){
+        this.isSelecting$ = this.status$.map((status) => status === FormStatus.Selection);
+        this.isLoading$ = this.status$.map((status) => status === FormStatus.Loading);
+        this.isFinished$ = this.status$.map((status) => status === FormStatus.Finished);
     }
-    return res;
-  }
+
+    changeListener($event): void {
+        if ($event.target.files.length > 0) {
+            this.data = {
+                file: $event.target.files[0],
+                content: '',
+                progress: 0,
+                isNull: false
+            };
+            $event.target.value = '';
+            this.upload();
+            this.status$.next(FormStatus.Loading);
+        } else {
+            this.data = {
+                file: null,
+                content: '',
+                progress: 0,
+                isNull: true
+            };
+        }
+    }
+
+    upload() {
+        let reader: FileReader = new FileReader();
+
+        reader.onload = ((e) => {
+            this.data.content = reader.result;
+            this.data.progress = 100;
+            this.status$.next(FormStatus.Finished);
+        });
+        reader.onerror = ((e) => {
+            console.log('Error encountered file upload');
+        });
+        reader.onprogress = ((e) => {
+            this.data.progress = e.loaded / e.total * 100;
+            console.log('progressed: ' + e.loaded / e.total * 100 + '% on file');
+        });
+        reader.onloadstart = ((e) => {
+            console.log('Reader started reading ' + this.data.file.name);
+        });
+
+        reader.readAsText(this.data.file);
+    }
+
+    submit(e: CSV) {
+        console.log(e);
+        this.result = e;
+    }
+
+    /**This method generates an number array containing all integers i with n <= i < m
+     *
+     * @param n lowest integer. !Warning: This integer is still contained in array.
+     * @param m highest integer. !Warning: This integer is not contained in array.
+     * @returns {number[]} Array {n,..,m-1}
+     */
+    range(n: number, m: number): number[] {
+        let res: number[] = [];
+        for (let i: number = n; i < m; i++) {
+            res.push(i);
+        }
+        return res;
+    }
 }
