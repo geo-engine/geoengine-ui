f1bab59e486bc949488afc115f9c744833971903
Christian Beilschmidt <beilschmidt@mathematik.uni-marburg.de>
2017-03-15 22:31:07 +0100

    bugfix: histogram operator
    closes T219

--- src/app/operators/dialogs/histogram-operator/histogram-operator.component.ts
+++ src/app/operators/dialogs/histogram-operator/histogram-operator.component.ts
@@ -1,6 +1,6 @@
 import {Component, ChangeDetectionStrategy, OnInit, OnDestroy, AfterViewInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {Subscription, ReplaySubject} from 'rxjs/Rx';
+import {Subscription, ReplaySubject, Observable} from 'rxjs/Rx';
 import {DataTypes, DataType} from '../../datatype.model';
 import {WaveValidators} from '../../../util/form.validators';
 import {MdDialogRef} from '@angular/material';
@@ -26,12 +26,13 @@ function isVectorLayer(layer: Layer<Symbology>): boolean {
 export class HistogramOperatorComponent implements OnInit, AfterViewInit, OnDestroy {
     // make public to template
     ResultTypes = ResultTypes;
-    isVectorLayer = isVectorLayer;
     //
 
     form: FormGroup;
 
     attributes$ = new ReplaySubject<Array<string>>(1);
+
+    isVectorLayer$: Observable<boolean>;
 
     private subscriptions: Array<Subscription> = [];
 
@@ -65,18 +66,18 @@ export class HistogramOperatorComponent implements OnInit, AfterViewInit, OnDest
         ;
 
         this.subscriptions.push(
-            this.form.controls['layer'].valueChanges.map(layer => {
-                // side effect!!!
-                this.form.controls['attribute'].setValue(undefined);
-
-                if (layer) {
-                    return layer.operator.attributes.filter((attribute: string) => {
-                        return DataTypes.ALL_NUMERICS.indexOf(layer.operator.dataTypes.get(attribute)) >= 0;
-                    }).toArray();
-                } else {
-                    return [];
-                }
-            }).subscribe(this.attributes$)
+            this.form.controls['layer'].valueChanges
+                .do(() => this.form.controls['attribute'].setValue(undefined))
+                .map(layer => {
+                    if (layer) {
+                        return layer.operator.attributes.filter((attribute: string) => {
+                            return DataTypes.ALL_NUMERICS.indexOf(layer.operator.dataTypes.get(attribute)) >= 0;
+                        }).toArray();
+                    } else {
+                        return [];
+                    }
+                })
+                .subscribe(this.attributes$)
         );
 
         this.subscriptions.push(
@@ -84,10 +85,15 @@ export class HistogramOperatorComponent implements OnInit, AfterViewInit, OnDest
                 .filter(rangeType => rangeType === 'custom')
                 .subscribe(() => this.form.controls['range'].updateValueAndValidity())
         );
+
+        this.isVectorLayer$ = this.form.controls['layer'].valueChanges.map(layer => isVectorLayer(layer));
     }
 
     ngAfterViewInit() {
-        setTimeout(() => this.form.updateValueAndValidity(), 0);
+        setTimeout(() => {
+            this.form.updateValueAndValidity();
+            this.form.controls['layer'].updateValueAndValidity();
+        });
     }
 
     ngOnDestroy() {
