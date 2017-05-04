4356e8e5928ff9a08c5138d606ab204286b62472
Julian <jmaerte@mathematik.uni-marburg.de>
2017-05-04 21:38:24 +0200

    Created new structural setup. Used template-driven forms to manage user inputs. TODO: Load components on view init, so it initiates with values from form. Modified csv-dialog component for new structure.

---
+++ src/app/operators/dialogs/csv/csv-config/csv-properties/csv-properties.component.ts
@@ -0,0 +1,155 @@
+/**
+ * Created by Julian on 04/05/2017.
+ */
+import {Component, ChangeDetectionStrategy, OnInit, AfterViewInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {Observable, BehaviorSubject, Subscription} from 'rxjs';
+import {IntervalFormat} from '../../interval.enum';
+
+enum FormStatus { DataProperties, SpatialProperties, TemporalProperties, TypingProperties, Loading }
+
+@Component({
+    selector: 'wave-csv-properties',
+    templateUrl: './csv-properties-template.component.html',
+    styleUrls: ['./csv-properties.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CsvPropertiesComponent implements OnInit, AfterViewInit {
+
+    delimiters: {def: string, value: string}[] = [
+        {def: 'TAB', value: '\t'},
+        {def: 'COMMA', value: ','},
+        {def: 'SEMICOLON', value: ';'}
+    ];
+
+    // http://man7.org/linux/man-pages/man3/strptime.3.html
+    timeFormats: Array<{display: string, value: string}> = [
+        {display: 'yyyy-MM-ddTHH:mm:ssZ', value: '%Y-%m-%dT%H:%M:%SZ'},
+        {display: 'yyyy-MM-ddTHH:mmZ', value: '%Y-%m-%dT%H:%MZ'},
+        {display: 'dd-MM-yyyy HH:mm:ss', value: '%d-%m-%Y %H:%M:%S'},
+        {display: 'dd.MM.yyyy HH:mm:ss', value: '%d.%m.%Y %H:%M:%S'},
+        {display: 'yyyy-MM-dd HH:mm:ssZ', value: '%Y-%m-%d %H:%M:%SZ'},
+        {display: 'yyyy-MM-dd HH:mmZ', value: '%Y-%m-%d %H:%MZ'},
+        {display: 'dd.MM.yyyy', value: '%d.%m.%Y'},
+        {display: 'yyyy-MM-dd', value: '%Y-%m-%d'},
+    ];
+    durationFormats: Array<{display: string, value: string}> = [
+        // {display: 'days', value: 'd'},
+        // {display: 'hours', value: 'h'},
+        {display: 'seconds', value: 'seconds'},
+    ];
+    intervalTypes: Array<{display: string, value: IntervalFormat}> = [
+        {display: '[Start,+inf)', value: IntervalFormat.StartInf},
+        {display: '[Start, End]', value: IntervalFormat.StartEnd},
+        {display: '[Start, Start+Duration]', value: IntervalFormat.StartDur},
+        {display: '[Start, Start+Constant]', value: IntervalFormat.StartConst},
+    ];
+    decimalSeparators: string[] = [',', '.'];
+    textQualifiers: string[] = ['"', '\''];
+    coordinateFormats: string[] = ['Degrees Minutes Seconds', 'Degrees Decimal Minutes', 'Decimal Degrees'];
+
+    dataProperties: FormGroup = new FormGroup({
+        delimiter: new FormControl(this.delimiters[1].value, Validators.required),
+        decimalSeparator: new FormControl(this.decimalSeparators[1], Validators.required),
+        isTextQualifier: new FormControl(false),
+        textQualifier: new FormControl({value: this.textQualifiers[0], disabled: true}, Validators.required),
+        isHeaderRow: new FormControl(true),
+        headerRow: new FormControl({value: 0, disabled: true}, Validators.required),
+    });
+    spatialProperties: FormGroup = new FormGroup({});
+    temporalProperties: FormGroup = new FormGroup({});
+    typingProperties:FormGroup = new FormGroup({});
+
+    dialogTitle: string;
+    subtitleDescription: string;
+
+    formStatus$: BehaviorSubject<FormStatus> = new BehaviorSubject<FormStatus>(FormStatus.Loading);
+    isDataProperties$: Observable<boolean>;
+    isSpatialProperties$: Observable<boolean>;
+    isTemporalProperties$: Observable<boolean>;
+    isTypingProperties$: Observable<boolean>;
+
+    actualPage$: BehaviorSubject<FormGroup> = new BehaviorSubject<FormGroup>(null);
+
+    private subscriptions: Array<Subscription> = [];
+
+    constructor() {
+        this.isDataProperties$ = this.formStatus$.map(status => status === FormStatus.DataProperties);
+        this.isSpatialProperties$ = this.formStatus$.map(status => status === FormStatus.SpatialProperties);
+        this.isTemporalProperties$ = this.formStatus$.map(status => status === FormStatus.TemporalProperties);
+        this.isTypingProperties$ = this.formStatus$.map(status => status === FormStatus.TypingProperties);
+    }
+
+    ngOnInit() {
+        this.subscriptions.push(
+            this.formStatus$.subscribe(status => {
+            switch (status) {
+                case FormStatus.DataProperties:
+                    this.dialogTitle = 'CSV Settings';
+                    this.subtitleDescription = 'Please specify the properties of your CSV file, e.g. the delimiter.';
+                    break;
+                case FormStatus.SpatialProperties:
+                    this.dialogTitle = 'Spatial Properties';
+                    this.subtitleDescription = 'In this step you can specify the spatial columns of your CSV file.';
+                    break;
+                case FormStatus.TemporalProperties:
+                    this.dialogTitle = 'Temporal Properties';
+                    this.subtitleDescription = 'This step allows you to specify temporal columns of your CSV file.';
+                    break;
+                case FormStatus.TypingProperties:
+                    this.dialogTitle = 'Typing Properties';
+                    this.subtitleDescription = 'You can specify the data types of the remaining columns here.';
+                    break;
+                case FormStatus.Loading:
+                /* falls through */
+                default:
+                    break;
+            }
+        }),
+        this.dataProperties.valueChanges.subscribe(data => {
+            console.log(data)
+        })
+        );
+        this.formStatus$.next(FormStatus.DataProperties);
+        this.actualPage$.next(this.dataProperties);
+    }
+
+    ngAfterViewInit() {
+        setTimeout(() => this.dataProperties.controls['delimiter'].updateValueAndValidity({
+            onlySelf: true,
+            emitEvent: true
+        }), 500);
+    }
+
+    nextPage() {
+        switch (this.formStatus$.getValue()) {
+            case FormStatus.DataProperties:
+                this.actualPage$.next(this.spatialProperties);
+                this.formStatus$.next(FormStatus.SpatialProperties);
+                break;
+            case FormStatus.SpatialProperties:
+                this.actualPage$.next(this.temporalProperties);
+                this.formStatus$.next(FormStatus.TemporalProperties);
+                break;
+            default:
+                this.actualPage$.next(this.typingProperties);
+                this.formStatus$.next(FormStatus.TypingProperties);
+        }
+    }
+
+    previousPage() {
+        switch (this.formStatus$.getValue()) {
+            case FormStatus.TemporalProperties:
+                this.actualPage$.next(this.spatialProperties);
+                this.formStatus$.next(FormStatus.SpatialProperties);
+                break;
+            case FormStatus.TypingProperties:
+                this.actualPage$.next(this.temporalProperties);
+                this.formStatus$.next(FormStatus.TemporalProperties);
+                break;
+            default:
+                this.actualPage$.next(this.dataProperties);
+                this.formStatus$.next(FormStatus.DataProperties);
+        }
+    }
+}
---
+++ src/app/operators/dialogs/csv/csv-dialog/csv-dialog/csv-dialog.component.ts
@@ -0,0 +1,174 @@
+import {Component, OnInit, ChangeDetectionStrategy, ViewChild} from '@angular/core';
+import {UploadData} from '../../csv-upload/csv-upload.component';
+import {CsvSourceType, CSVParameters} from '../../../../types/csv-source-type.model';
+import {Operator} from '../../../../operator.model';
+import {CSV, CsvConfigComponent} from '../../csv-config/csv-config.component';
+import {ResultTypes} from '../../../../result-type.model';
+import {UserService} from '../../../../../users/user.service';
+import {LayerService} from '../../../../../layers/layer.service';
+import {AbstractVectorSymbology, ClusteredPointSymbology, SimpleVectorSymbology} from '../../../../../layers/symbology/symbology.model';
+import {VectorLayer} from '../../../../../layers/layer.model';
+import {MappingQueryService} from '../../../../../queries/mapping-query.service';
+import {RandomColorService} from '../../../../../util/services/random-color.service';
+import {MdDialogRef} from '@angular/material';
+import {BehaviorSubject} from 'rxjs/Rx';
+import {Projections} from '../../../../projection.model';
+import {ProjectionType} from '../../../../types/projection-type.model';
+import {IntervalFormat} from '../../interval.enum';
+import {CsvPropertiesComponent} from '../../csv-config/csv-properties/csv-properties.component';
+
+@Component({
+    selector: 'wave-csv-dialog',
+    templateUrl: './csv-dialog.component.html',
+    styleUrls: ['./csv-dialog.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CsvDialogComponent implements OnInit {
+
+    IntervalFormat = IntervalFormat;
+    @ViewChild(CsvPropertiesComponent) csvConfig;
+    data: UploadData = undefined;
+    uploading$ = new BehaviorSubject(false);
+
+    constructor(private userService: UserService,
+                private layerService: LayerService,
+                private mappingQueryService: MappingQueryService,
+                private randomColorService: RandomColorService,
+                private dialogRef: MdDialogRef<CsvDialogComponent>) {
+    }
+
+    ngOnInit() {
+    }
+
+    submit(config: CSV) {
+        // TODO: refactor most of this
+        const fieldSeparator = config.delimitter;
+        const geometry = 'xy';
+        const time = config.intervalString;
+        const time1Format = config.startFormat;
+        const time2Format = config.endFormat;
+        const header = config.isHeaderRow ? 0 : config.header;
+        const columnX = config.header[config.xCol];
+        const columnY = config.header[config.yCol];
+        const time1 = config.header[config.startCol];
+        const time2 = config.header[config.endCol];
+        const numericColumns = config.header.filter((name, index) => config.isNumberArr[index]);
+        const textualColumns = config.header.filter((name, index) => !config.isNumberArr[index]);
+        const onError = 'skip';
+
+        let parameters: CSVParameters = {
+            fieldSeparator: fieldSeparator,
+            geometry: geometry,
+            time: time as ('none' | 'start+inf' | 'start+end' | 'start+duration'),
+            header: header,
+            columns: {
+                x: columnX,
+                y: columnY,
+                numeric: numericColumns,
+                textual: textualColumns,
+            },
+            onError: onError,
+        };
+
+        // filter out geo columns
+        function removeIfExists(array: Array<string>, name: string) {
+            const index = array.indexOf(name);
+            if (index >= 0) {
+                array.splice(index, 1);
+            }
+        }
+
+        removeIfExists(parameters.columns.textual, columnX);
+        removeIfExists(parameters.columns.numeric, columnX);
+        removeIfExists(parameters.columns.textual, columnY);
+        removeIfExists(parameters.columns.numeric, columnY);
+
+        if (time !== 'none') {
+            parameters.timeFormat = {
+                time1: {
+                    format: 'custom',
+                    customFormat: time1Format,
+                }
+            };
+            parameters.columns.time1 = time1;
+
+            removeIfExists(parameters.columns.textual, time1);
+            removeIfExists(parameters.columns.numeric, time1);
+
+            if (time.indexOf('end') >= 0) {
+                parameters.timeFormat.time2 = {
+                    format: 'custom',
+                    customFormat: time2Format,
+                };
+                parameters.columns.time2 = time2;
+
+                removeIfExists(parameters.columns.textual, time2);
+                removeIfExists(parameters.columns.numeric, time2);
+            }
+            if (time.indexOf('duration') >= 0) {
+                // TODO: refactor for other formats
+                parameters.timeFormat.time2 = {
+                    format: time2Format as 'seconds',
+                };
+                parameters.columns.time2 = time2;
+
+                removeIfExists(parameters.columns.textual, time2);
+                removeIfExists(parameters.columns.numeric, time2);
+            }
+        }
+
+        const csvSourceType = new CsvSourceType({
+            dataURI: 'data:text/plain,' + config.content,
+            parameters: parameters,
+        });
+
+        const operator = new Operator({
+            operatorType: csvSourceType,
+            resultType: ResultTypes.POINTS,
+            projection: config.spatialRefSys,
+        }).getProjectedOperator(Projections.WGS_84);
+
+        this.uploading$.next(true);
+
+        // console.log("SAVE", config, csvSourceType);
+        this.userService.addFeatureToDB(config.layerName, operator)
+            .subscribe(data => {
+                this.uploading$.next(false);
+
+                this.addLayer(data);
+
+                this.dialogRef.close();
+            });
+    }
+
+    private addLayer(entry: {name: string, operator: Operator}) {
+        const color = this.randomColorService.getRandomColor();
+        let symbology: AbstractVectorSymbology;
+        let clustered: boolean;
+
+        if (entry.operator.resultType === ResultTypes.POINTS) {
+            symbology = new ClusteredPointSymbology({
+                fillRGBA: color,
+            });
+            clustered = true;
+        } else {
+            symbology = new SimpleVectorSymbology({
+                fillRGBA: color,
+            });
+            clustered = false;
+        }
+
+        const layer = new VectorLayer({
+            name: entry.name,
+            operator: entry.operator,
+            symbology: symbology,
+            data: this.mappingQueryService.getWFSDataStreamAsGeoJsonFeatureCollection({
+                operator: entry.operator,
+                clustered: clustered,
+            }),
+            provenance: this.mappingQueryService.getProvenanceStream(entry.operator),
+            clustered: clustered,
+        });
+        this.layerService.addLayer(layer);
+    }
+}
