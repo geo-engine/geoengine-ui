3920886577c32534c3e508da380b813c92fe5944
Julian Maerte <maertej@students.uni-marburg.de>
2019-04-16 15:42:21 +0200

    Testing if the temporal properties are disabled for csv inputs with only 2 columns. Next step: Are temporal properties getting enabled when there are more columns after data properties change.

--- src/app/operators/dialogs/csv/csv-dialog/csv-dialog.component.spec.ts
+++ src/app/operators/dialogs/csv/csv-dialog/csv-dialog.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing'
 
 import { CsvDialogComponent } from './csv-dialog.component';
 import {CsvPropertiesService} from './csv.properties.service';
-import {CsvPropertiesComponent} from '../csv-config/csv-properties/csv-properties.component';
+import {CsvPropertiesComponent, FormStatus} from '../csv-config/csv-properties/csv-properties.component';
 import {CsvTableComponent} from '../csv-config/csv-table/csv-table.component';
 import {CsvUploadComponent} from '../csv-upload/csv-upload.component';
 import {MaterialModule} from '../../../../material.module';
@@ -31,80 +31,121 @@ class MockProjectService {
 }
 
 describe('CsvDialogComponent', () => {
-  let component: CsvDialogComponent;
-  // let propertiesService: CsvPropertiesService;
-  let fixture: ComponentFixture<CsvDialogComponent>;
+    let component: CsvDialogComponent;
+    // let propertiesService: CsvPropertiesService;
+    let fixture: ComponentFixture<CsvDialogComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-          CsvDialogComponent,
-          CsvPropertiesComponent,
-          CsvTableComponent,
-          CsvUploadComponent,
-          DialogSectionHeadingComponent,
-          DialogHeaderComponent
-      ],
-      imports: [
-          MaterialModule,
-          FormsModule,
-          MatDialogModule,
-          ReactiveFormsModule,
-          BrowserAnimationsModule
-      ],
-      providers: [
-          CsvPropertiesService,
-          RandomColorService,
-          {provide: ProjectService, useClass: MockProjectService},
-          {provide: UserService, useClass: MockUserService},
-          {provide: MatDialogRef, useValue: {}}
-      ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                CsvDialogComponent,
+                CsvPropertiesComponent,
+                CsvTableComponent,
+                CsvUploadComponent,
+                DialogSectionHeadingComponent,
+                DialogHeaderComponent
+            ],
+            imports: [
+                MaterialModule,
+                FormsModule,
+                MatDialogModule,
+                ReactiveFormsModule,
+                BrowserAnimationsModule
+            ],
+            providers: [
+                CsvPropertiesService,
+                RandomColorService,
+                {provide: ProjectService, useClass: MockProjectService},
+                {provide: UserService, useClass: MockUserService},
+                {provide: MatDialogRef, useValue: {}}
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CsvDialogComponent);
-    component = fixture.componentInstance;
-    // propertiesService = TestBed.get(CsvPropertiesService);
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CsvDialogComponent);
+        component = fixture.componentInstance;
+        // propertiesService = TestBed.get(CsvPropertiesService);
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  // it('should parse correctly', inject([CsvPropertiesService], (propertiesService: CsvPropertiesService) => {
-  //   const modifiedDate = new Date();
-  //   component.data = {
-  //       file: new File(['"a,b",c,d\n"1,2",3,4'], 'test-file.csv', {lastModified : modifiedDate.getDate(), type: 'csv'}),
-  //       content: '"a,b",c,d\n"1,2",3,4',
-  //       progress: 100,
-  //       isNull: false
-  //   };
-  //   component.uploading$.next(false);
-  //   propertiesService.changeDataProperties({
-  //         delimiter: ',',
-  //         decimalSeparator: '.',
-  //         isTextQualifier: false,
-  //         textQualifier: '"',
-  //         isHeaderRow: true,
-  //         headerRow: 0,
-  //   });
-  //   fixture.detectChanges();
-  //   component.csvTable._changeDetectorRef.detectChanges();
-  //   const table_rows = document.getElementsByTagName('tr');
-  //   expect(table_rows[table_rows.length - 1].getElementsByTagName('td').length).toBe(5);
-  //   // propertiesService.changeDataProperties({
-  //   //     delimiter: ',',
-  //   //     decimalSeparator: '.',
-  //   //     isTextQualifier: false,
-  //   //     textQualifier: '"',
-  //   //     isHeaderRow: true,
-  //   //     headerRow: 0,
-  //   // });
-  //   // fixture.detectChanges();
-  //   // const table_rows_notext = document.getElementsByTagName('tr');
-  //   // expect(table_rows_notext[table_rows_notext.length - 1].getElementsByTagName('td').length).toBe(5);
-  // }));
+    it('should update table', (done) => {
+        const modifiedDate = new Date();
+        component.data = {
+            file: new File(['"a,b",c,d\n"1,2",3,4'], 'test-file.csv', {lastModified : modifiedDate.getDate(), type: 'csv'}),
+            content: '"a,b",c,d\n"1,2",3,4',
+            progress: 100,
+            isNull: false
+        };
+        component.uploading$.next(false);
+        fixture.detectChanges();
+
+        component.csvProperties.dataProperties.patchValue({isTextQualifier: true});
+        fixture.detectChanges();
+        component.csvTable._changeDetectorRef.detectChanges();
+        fixture.detectChanges();
+        setTimeout(() => {
+            fixture.detectChanges();
+            const table_rows = document.getElementsByTagName('tr');
+            expect(table_rows[table_rows.length - 1].getElementsByTagName('td').length).toBe(7);
+            done();
+        }, 200);
+    });
+
+    it('should disable/enable temporal properties', () => {
+        const modifiedDate = new Date();
+        component.data = {
+            file: new File(['"a,b",c\n"1,2",3'], 'test-file.csv', {lastModified : modifiedDate.getDate(), type: 'csv'}),
+            content: 'a,b\n1,2',
+            progress: 100,
+            isNull: false
+        };
+        component.uploading$.next(false);
+        fixture.detectChanges();
+        component.csvProperties.formStatus$.next(FormStatus.TemporalProperties);
+        fixture.detectChanges();
+        expect(component.csvProperties.temporalProperties.get('isTime').disabled).toBeTruthy();
+        // component.csvProperties.dataProperties.get('isTextQualifier').setValue(false);
+        // fixture.detectChanges();
+        // expect(component.csvProperties.temporalProperties.get('isTime').disabled).toBeFalsy();
+    });
+
+    // it('should parse correctly', inject([CsvPropertiesService], (propertiesService: CsvPropertiesService) => {
+    //   const modifiedDate = new Date();
+    //   component.data = {
+    //       file: new File(['"a,b",c,d\n"1,2",3,4'], 'test-file.csv', {lastModified : modifiedDate.getDate(), type: 'csv'}),
+    //       content: '"a,b",c,d\n"1,2",3,4',
+    //       progress: 100,
+    //       isNull: false
+    //   };
+    //   component.uploading$.next(false);
+    //   propertiesService.changeDataProperties({
+    //         delimiter: ',',
+    //         decimalSeparator: '.',
+    //         isTextQualifier: false,
+    //         textQualifier: '"',
+    //         isHeaderRow: true,
+    //         headerRow: 0,
+    //   });
+    //   fixture.detectChanges();
+    //   component.csvTable._changeDetectorRef.detectChanges();
+    //   const table_rows = document.getElementsByTagName('tr');
+    //   expect(table_rows[table_rows.length - 1].getElementsByTagName('td').length).toBe(5);
+    //   // propertiesService.changeDataProperties({
+    //   //     delimiter: ',',
+    //   //     decimalSeparator: '.',
+    //   //     isTextQualifier: false,
+    //   //     textQualifier: '"',
+    //   //     isHeaderRow: true,
+    //   //     headerRow: 0,
+    //   // });
+    //   // fixture.detectChanges();
+    //   // const table_rows_notext = document.getElementsByTagName('tr');
+    //   // expect(table_rows_notext[table_rows_notext.length - 1].getElementsByTagName('td').length).toBe(5);
+    // }));
 });
