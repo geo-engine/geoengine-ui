8cce72390b8c9aadcd3a8d120c5ffdaaae629dd8
Julian <jmaerte@mathematik.uni-marburg.de>
2019-06-23 19:09:20 +0200

    Added some genericness to the csv table tests. An array of formal test cases is used to be easily expandable. A function returns the expected behavior for the given case.

--- src/app/operators/dialogs/csv/csv-config/csv-table/csv-table.component.spec.ts
+++ src/app/operators/dialogs/csv/csv-config/csv-table/csv-table.component.spec.ts
@@ -10,110 +10,177 @@ import { HEADER_TABLE_ID, BODY_TABLE_ID } from './csv-table.component';
 import 'hammerjs';
 import {TestIdComponentDirective} from '../../../../../spec/test-id-component.directive';
 import {FormStatus} from '../csv-properties/csv-properties.component';
+import {Map} from 'immutable';
+
+interface Setting {
+    delimiter: string,
+    isTextQualifier: boolean
+}
+
+interface TestCase {
+    case_name: string,
+    inputs: {
+        data: {
+            file: File,
+            content: string,
+            progress: number,
+            isNull: boolean
+        },
+        cellSpacing: number,
+        linesToParse: number
+    },
+    behavior(setting: Setting): {columns: number}
+}
 
 describe('Component: CsvTableComponent', () => {
+
     let service: CsvPropertiesService;
     let fixture: ComponentFixtureSpecHelper<CsvTableComponent>;
     let component: CsvTableComponent;
 
-    beforeEach(() => {
-        fixture = new ComponentFixtureSpecHelper<CsvTableComponent>({
-            declarations: [
-                CsvTableComponent,
-                TestIdComponentDirective
-            ],
-            imports: [
-                MaterialModule,
-                FormsModule,
-                BrowserAnimationsModule
-            ],
-            providers: [
-                CsvPropertiesService,
-                ChangeDetectorRef
-            ]
-        }, CsvTableComponent, {
-            data: {
-                file: new File(['"a,b",c,d\n"1,2",3,4'], 'test-file.csv', {lastModified : (new Date()).getDate(), type: 'csv'}),
-                content: '"a,b",c,d\n"1,2",3,4',
-                progress: 100,
-                isNull: false
+    let test_cases: TestCase[] = [
+        {
+            case_name: '3 col to 4 col',
+            inputs: {
+                data: {
+                    file: new File(['"a,b",c,d\n"1,2",3,4'], 'test-file.csv', {lastModified : (new Date()).getDate(), type: 'csv'}),
+                    content: '"a,b",c,d\n"1,2",3,4',
+                    progress: 100,
+                    isNull: false
+                },
+                cellSpacing: 10,
+                linesToParse: 15
             },
-            cellSpacing: 10,
-            linesToParse: 15
-        });
-
-        component = fixture.getComponentInstance();
-        service = fixture.getInjected(CsvPropertiesService);
-    });
-
-    it('should parse and update correctly', () => {
-        service.changeDataProperties({
-            delimiter: ',',
-            decimalSeparator: '.',
-            isTextQualifier: false,
-            textQualifier: '"',
-            isHeaderRow: true,
-            headerRow: 0,
-        });
-        fixture.detectChanges();
-        expect(getHeader().length).toBe(4);
-        expect(numberOfTableColumns()).toBe(4);
-        service.changeDataProperties({
-            delimiter: ',',
-            decimalSeparator: '.',
-            isTextQualifier: true,
-            textQualifier: '"',
-            isHeaderRow: true,
-            headerRow: 0,
-        });
-        fixture.detectChanges();
-        expect(getHeader().length).toBe(3);
-        expect(numberOfTableColumns()).toBe(3);
-    });
-
-    it('resizes table columns and synchronizes header and body widths when using custom headers', async(async () => {
-        component.data.content = '"a,b",c,dddddddddddddd\n"1,2",3,4';
-        const TABLE_WIDTH = 4;
-        service.changeDataProperties({
-            delimiter: ',',
-            decimalSeparator: '.',
-            isTextQualifier: false,
-            textQualifier: '"',
-            isHeaderRow: false,
-            headerRow: 0,
-        });
-        fixture.detectChanges();
-        let header_row = headerRow();
-        let body_row = bodyRow();
-        expect(tableWidthWithoutSpacers(header_row)).toBe(TABLE_WIDTH);
-        expect(tableWidthWithoutSpacers(body_row)).toBe(TABLE_WIDTH);
-        await fixture.whenStable();
-        fixture.detectChanges();
-        for (let i = 0; i < header_row.length; i++) {
-            expect(clientWidth(header_row[i])).toBe(
-                i % 2 === 0 ?
-                    component.cellSizes[i] :
-                    component.cellSpacing
-            ); // Test whether or not the table resizes to the given values.
-            expect(clientWidth(header_row[i])).toBe(clientWidth(body_row[i]));
-            // Test if body and header have synchronized cell widths.
+            behavior(setting: Setting) {
+                return {columns: setting.isTextQualifier ? 3 : 4}
+            }
+        },
+        {
+            case_name: '3 col to 4 col (long headers)',
+            inputs: {
+                data: {
+                    file: new File(['"aaaaaaaaaaaaaaaaaaaa,bbbbbbbbbbbbbbbbbb",ccccccccccccccc,dddddddddddddd\n"1,2",3,4'],
+                        'test-file.csv',
+                        {lastModified : (new Date()).getDate(), type: 'csv'}
+                    ),
+                    content: '"aaaaaaaaaaaaaaaaaaaa,bbbbbbbbbbbbbbbbbb",ccccccccccccccc,dddddddddddddd\n"1,2",3,4',
+                    progress: 100,
+                    isNull: false
+                },
+                cellSpacing: 10,
+                linesToParse: 15
+            },
+            behavior(setting: Setting) {
+                return {columns: setting.isTextQualifier ? 3 : 4}
+            }
         }
-    }));
+    ];
 
-    it('resizes table when entering typing properties', async(async () => {
-        service.changeFormStatus(FormStatus.TypingProperties);
-        fixture.detectChanges();
-        let header_row = headerRow();
-        let body_row = bodyRow();
-        let typing_row = typingRow();
-        await fixture.whenStable();
-        fixture.detectChanges();
-        for (let i = 0; i < header_row.length; i++) {
-            expect(clientWidth(header_row[i])).toBe(clientWidth(body_row[i]));
-            expect(clientWidth(header_row[i])).toBe(clientWidth(typing_row[i]));
-            // Test if body, header and typing have synchronized cell widths.
-        }
-    }));
+    for (let i = 0; i < test_cases.length; i++) {
+        let test_case = test_cases[i];
+        describe('Test Case: ' + test_case.case_name, () => {
+            beforeEach(() => {
+                fixture = new ComponentFixtureSpecHelper<CsvTableComponent>({
+                    declarations: [
+                        CsvTableComponent,
+                        TestIdComponentDirective
+                    ],
+                    imports: [
+                        MaterialModule,
+                        FormsModule,
+                        BrowserAnimationsModule
+                    ],
+                    providers: [
+                        CsvPropertiesService,
+                        ChangeDetectorRef
+                    ]
+                }, CsvTableComponent, test_case.inputs);
+
+                component = fixture.getComponentInstance();
+                service = fixture.getInjected(CsvPropertiesService);
+            });
+
+            it('should parse and update correctly', () => {
+                console.log(test_case.behavior);
+                service.changeDataProperties({
+                    delimiter: ',',
+                    decimalSeparator: '.',
+                    isTextQualifier: false,
+                    textQualifier: '"',
+                    isHeaderRow: true,
+                    headerRow: 0,
+                });
+                let TABLE_WIDTH = test_case.behavior({
+                    delimiter: ',',
+                    isTextQualifier: false
+                }).columns;
+                fixture.detectChanges();
+                expect(getHeader().length).toBe(TABLE_WIDTH);
+                expect(numberOfTableColumns()).toBe(TABLE_WIDTH);
+                service.changeDataProperties({
+                    delimiter: ',',
+                    decimalSeparator: '.',
+                    isTextQualifier: true,
+                    textQualifier: '"',
+                    isHeaderRow: true,
+                    headerRow: 0,
+                });
+                TABLE_WIDTH = test_case.behavior({
+                    delimiter: ',',
+                    isTextQualifier: true
+                }).columns;
+                fixture.detectChanges();
+                expect(getHeader().length).toBe(TABLE_WIDTH);
+                expect(numberOfTableColumns()).toBe(TABLE_WIDTH);
+            });
+
+            it('resizes table columns and synchronizes header and body widths when using custom headers', async(async () => {
+                service.changeDataProperties({
+                    delimiter: ',',
+                    decimalSeparator: '.',
+                    isTextQualifier: false,
+                    textQualifier: '"',
+                    isHeaderRow: false,
+                    headerRow: 0,
+                });
+                const TABLE_WIDTH = test_case.behavior({
+                    delimiter: ',',
+                    isTextQualifier: false
+                }).columns;
+                fixture.detectChanges();
+                let header_row = headerRow();
+                let body_row = bodyRow();
+                expect(tableWidthWithoutSpacers(header_row)).toBe(TABLE_WIDTH);
+                expect(tableWidthWithoutSpacers(body_row)).toBe(TABLE_WIDTH);
+                await fixture.whenStable();
+                fixture.detectChanges();
+                for (let i = 0; i < header_row.length; i++) {
+                    expect(clientWidth(header_row[i])).toBe(
+                        i % 2 === 0 ?
+                            component.cellSizes[i] :
+                            component.cellSpacing
+                    ); // Test whether or not the table resizes to the given values.
+                    expect(clientWidth(header_row[i])).toBe(clientWidth(body_row[i]));
+                    // Test if body and header have synchronized cell widths.
+                }
+            }));
+
+            it('resizes table when entering typing properties', async(async () => {
+                service.changeFormStatus(FormStatus.TypingProperties);
+                fixture.detectChanges();
+                let header_row = headerRow();
+                let body_row = bodyRow();
+                let typing_row = typingRow();
+                await fixture.whenStable();
+                fixture.detectChanges();
+                for (let i = 0; i < header_row.length; i++) {
+                    expect(clientWidth(header_row[i])).toBe(clientWidth(body_row[i]));
+                    expect(clientWidth(header_row[i])).toBe(clientWidth(typing_row[i]));
+                    // Test if body, header and typing have synchronized cell widths.
+                }
+            }));
+        });
+    }
 
     function getHeader(): {value: string}[] {
         return component.header;
