3a7e1ef19bab1b1b36ae3afcd2ef659964f5d5e8
Julian Maerte <jmaerte@mathematik.uni-marburg.de>
2019-08-28 13:04:19 +0200

    Further experiments with HttpTestingController for User Service.

--- src/app/users/user.service.spec.ts
+++ src/app/users/user.service.spec.ts
@@ -9,45 +9,74 @@ import {NotificationService} from '../notification.service';
 
 describe('Service: User Service', () => {
 
-    configureWaveTesting(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                {provide: Config, useClass: MockConfig},
-                UserService,
-                {provide: NotificationService, useClass: MockNotificationService}
-            ],
-            imports: [
-                HttpClientTestingModule
-            ]
-        });
-        this.service = TestBed.get(UserService);
-        this.backend = new MockBackend(TestBed.get(HttpTestingController), MockConfig.MOCK_URL);
-    });
+    // configureWaveTesting(() => {
+    //     TestBed.configureTestingModule({
+    //         providers: [
+    //             {provide: Config, useClass: MockConfig},
+    //             UserService,
+    //             {provide: NotificationService, useClass: MockNotificationService}
+    //         ],
+    //         imports: [
+    //             HttpClientTestingModule
+    //         ]
+    //     });
+    //     this.service = TestBed.get(UserService);
+    //     this.http = TestBed.get(HttpTestingController);
+    //     this.backend = new MockBackend(this.http, MockConfig.MOCK_URL);
+    //
+    //     this.http.match((req) => req.request === 'info').forEach(req => req.flush(req.session === '' ? {
+    //         email: 'guest',
+    //         externalid: '',
+    //         realname: 'guest',
+    //         result: true,
+    //         username: 'guest'
+    //     } : {
+    //         email: 'test',
+    //         externalid: '',
+    //         realname: 'test',
+    //         result: true,
+    //         username: 'test'
+    //     }));
+    //     this.http.match((req) => req.request === 'login').forEach(req => req.flush(
+    //         (req.username === MockConfig.MOCK_USER.GUEST.NAME && req.password === MockConfig.MOCK_USER.GUEST.PASSWORD) ?
+    //         {result: true, session: ''} : {result: true, session: '0'})
+    //     );
+    //     // const isValidReq = this.http.match((req) => req.request === 'login' && req.sessionToken === '');
+    //     // expect(isValidReq.length).toBe(1);
+    //     // isValidReq.forEach(
+    //     //     req => req.flush({result: false})
+    //     // );
+    //     // const guestLoginReq = this.http.match((req) => req.user === MockConfig.MOCK_USER.GUEST.NAME &&
+    //     //     req.password === MockConfig.MOCK_USER.GUEST.PASSWORD
+    //     // );
+    //     // expect(guestLoginReq.length).toBe(1);
+    //     // guestLoginReq.forEach(req => req.flush({result: true, session: ''}));
+    // });
 
-    // it('detects wrong credentials and behaves properly', () => {
+    // it('detects wrong credentials and sets session properly', () => {
+    //     this.http.match((req) => req.body.includes('username=guest'))
+    //         .forEach(req => req.flush({result: true, session: ''}));
     //     this.service.login({user: 'test', password: 'test'})
     //         .subscribe((login_response) => {
-    //             expect(login_response).toBe({result: 'UserDB: username or password wrong'})
+    //             expect(login_response).toBe(false);
     //
-    //             expect(this.service.getSession()).toBe({
-    //                 user: MockConfig.MOCK_USER.GUEST.NAME,
-    //                 sessionToken: ''
-    //             });
+    //             expect(this.service.getSession().user).toBe(MockConfig.MOCK_USER.GUEST.NAME);
+    //             expect(this.service.getSession().sessionToken).toBe('');
     //         }
     //     );
     //
     //     this.backend.testLogin();
     // });
     //
-    // it('detects right credentials and behaves properly', () => {
+    // it('detects right credentials and sets session properly', () => {
+    //     this.http.match((req) => req.body.includes('username=guest'))
+    //         .forEach(req => req.flush({result: true, session: ''}));
     //     this.service.login({user: 'test', password: 'test_pw'})
     //         .subscribe((login_response) => {
-    //                 expect(login_response).toBe({result: true, session: 'mockSessionToken'})
+    //                 expect(login_response).toBe(true);
     //
-    //                 expect(this.service.getSession()).toBe({
-    //                     user: 'test',
-    //                     sessionToken: 'mockSessionToken'
-    //                 });
+    //                 expect(this.service.getSession().user).toBe('test');
+    //                 expect(this.service.getSession().sessionToken).toBe('mockSessionToken')
     //             }
     //         );
     //
@@ -69,12 +98,16 @@ class MockBackend {
     }
 
     public testLogin() {
+        // this.httpTestingController.match((req) => true).forEach(req => console.log(req));
         const request = this.httpTestingController.expectOne(this.URL);
-        if (this.logins.indexOf(request.request.body as {user: string, password: string}) >= 0) {
-            request.flush({result: true, session: 'mockSessionToken'});
-        } else {
-            request.flush({result: 'UserDB: username or password wrong'});
+        for (let login of this.logins) {
+            if (request.request.body.includes('username=' + login.user + '&')
+                && request.request.body.includes('password=' + login.password + '&')) {
+                request.flush({result: true, session: 'mockSessionToken'});
+                return;
+            }
         }
+        request.flush({result: 'UserDB: username or password wrong'});
     }
 }
 
