d60e839da8a0c1e1ceaaf9d2baca8c892057aeec
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2020-02-20 10:49:19 +0100

    add layer histogram to the color map editor

--- src/app/layers/symbology/symbology-raster/symbology-raster-mapping-colorizer.component.ts
+++ src/app/layers/symbology/symbology-raster/symbology-raster-mapping-colorizer.component.ts
@@ -1,10 +1,32 @@
-import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy, OnChanges, SimpleChanges, OnInit} from '@angular/core';
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    ChangeDetectionStrategy,
+    OnChanges,
+    SimpleChanges,
+    OnInit,
+    AfterViewInit, OnDestroy, ElementRef
+} from '@angular/core';
 
-import {MappingColorizerRasterSymbology} from '../symbology.model';
+import {AbstractVectorSymbology, MappingColorizerRasterSymbology} from '../symbology.model';
 import {MatSliderChange} from '@angular/material';
 import {ColorizerData} from '../../../colors/colorizer-data.model';
 import {ColorBreakpoint} from '../../../colors/color-breakpoint.model';
-import {RasterLayer} from '../../layer.model';
+import {RasterLayer, VectorLayer} from '../../layer.model';
+import {BehaviorSubject, combineLatest as observableCombineLatest, Subscription} from 'rxjs';
+import {HistogramData} from '../../../plots/histogram/histogram.component';
+import {LayoutService} from '../../../layout.service';
+import {ProjectService} from '../../../project/project.service';
+import {Operator} from '../../../operators/operator.model';
+import {HistogramType} from '../../../operators/types/histogram-type.model';
+import {DataType} from '../../../operators/datatype.model';
+import {Unit} from '../../../operators/unit.model';
+import {first} from 'rxjs/operators';
+import {ResultTypes} from '../../../operators/result-type.model';
+import {MappingQueryService} from '../../../queries/mapping-query.service';
+import {MapService} from '../../../map/map.service';
 
 @Component({
     selector: 'wave-symbology-raster-mapping-colorizer',
@@ -12,14 +34,39 @@ import {RasterLayer} from '../../layer.model';
     styleUrls: ['symbology-raster-mapping-colorizer.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SymbologyRasterMappingColorizerComponent implements OnChanges, OnInit {
+export class SymbologyRasterMappingColorizerComponent implements OnChanges, OnInit, AfterViewInit, OnDestroy {
 
     @Input() layer: RasterLayer<MappingColorizerRasterSymbology>;
     @Output() symbologyChanged: EventEmitter<MappingColorizerRasterSymbology> = new EventEmitter<MappingColorizerRasterSymbology>();
 
     symbology: MappingColorizerRasterSymbology;
 
-    constructor() {
+    layerMinValue: number | undefined = undefined;
+    layerMaxValue: number | undefined = undefined;
+    layerHistogramData$: BehaviorSubject<HistogramData> = new BehaviorSubject(undefined);
+    layerHistogramDataLoading$ = new BehaviorSubject(false);
+    layerHistogramWidth: number;
+    layerHistogramHeight: number;
+    private subscriptions: Array<Subscription> = [];
+
+    constructor(
+        public projectService: ProjectService,
+        public elementRef: ElementRef,
+        public mappingQueryService: MappingQueryService,
+        public mapService: MapService
+    ) {
+    }
+
+    updateLayerMinValue(min: number) {
+        if (this.layerMinValue !== min) {
+            this.layerMinValue = min;
+        }
+    }
+
+    updateLayerMaxValue(max: number) {
+        if (this.layerMaxValue !== max) {
+            this.layerMaxValue = max;
+        }
     }
 
     updateOpacity(event: MatSliderChange) {
@@ -29,7 +76,6 @@ export class SymbologyRasterMappingColorizerComponent implements OnChanges, OnIn
 
     updateOverflowColor(event: ColorBreakpoint) {
         if (event && !event.equals(this.symbology.overflowColor)) {
-            // console.log("updateOverflowColor");
             this.symbology.overflowColor = event;
             this.update();
         }
@@ -37,7 +83,6 @@ export class SymbologyRasterMappingColorizerComponent implements OnChanges, OnIn
 
     updateNoDataColor(event: ColorBreakpoint) {
         if (event && !event.equals(this.symbology.noDataColor)) {
-            // console.log("updateNoDataColor");
             this.symbology.noDataColor = event;
             this.update();
         }
@@ -65,6 +110,11 @@ export class SymbologyRasterMappingColorizerComponent implements OnChanges, OnIn
         this.symbology = this.layer.symbology;
     }
 
+    updateLayerMinMaxFromColorizer() {
+        this.updateLayerMaxValue(this.colorizerMinValue);
+        this.updateLayerMaxValue(this.colorizerMaxValue);
+    }
+
     update() {
         this.symbologyChanged.emit(this.symbology.clone());
     }
@@ -75,15 +125,69 @@ export class SymbologyRasterMappingColorizerComponent implements OnChanges, OnIn
             switch (propName) {
                 case 'layer':
                     this.updateSymbologyFromLayer();
+                    this.updateLayerMinMaxFromColorizer();
+                    this.updateLayerHistogram();
                     break;
                 default: // DO NOTHING
-                // console.log('SymbologyRasterMappingColorizerComponent', 'ngOnChanges', 'default: ', propName)
-
             }
         }
     }
 
     ngOnInit(): void {
         this.updateSymbologyFromLayer();
+        this.updateLayerMinMaxFromColorizer();
+        this.updateLayerHistogram();
+    }
+
+    ngAfterViewInit() {
+        // calculate size for histogram
+        const panelStyle = getComputedStyle(this.elementRef.nativeElement.querySelector('mat-expansion-panel'));
+        const panelWidth = parseInt(panelStyle.width, 10) - 2 * LayoutService.remInPx();
+        const paneleight = parseInt(panelStyle.height, 10) - 2 * LayoutService.remInPx();
+        this.layerHistogramWidth = panelWidth;
+        this.layerHistogramHeight = Math.max(paneleight / 3, panelWidth / 3);
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(subscription => subscription.unsubscribe());
+    }
+
+    updateLayerHistogram() {
+        this.layerHistogramData$.next(undefined);
+
+        const histogramOperator = new Operator({
+            operatorType: new HistogramType({
+                attribute: 'value',
+                range: 'data',
+            }),
+            resultType: ResultTypes.PLOT,
+            projection: this.layer.operator.projection,
+            attributes: [],
+            dataTypes: new Map<string, DataType>(),
+            units: new Map<string, Unit>(),
+            rasterSources: [this.layer.operator],
+        });
+
+        this.layerHistogramDataLoading$.next(true);
+        const sub = observableCombineLatest(this.projectService.getTimeStream().pipe(
+            first()),
+            this.projectService.getProjectionStream().pipe(
+                first()
+            )
+        )
+            .subscribe(([projectTime, projection]) => {
+                this.mappingQueryService.getPlotData({
+                    operator: histogramOperator,
+                    time: projectTime,
+                    extent: this.mapService.getViewportSize().extent,
+                    projection: projection,
+                }).subscribe(data => {
+                    this.layerHistogramData$.next(data as HistogramData);
+                    this.layerHistogramDataLoading$.next(false);
+                });
+            });
+
+
+        this.subscriptions.push(sub);
     }
 }
