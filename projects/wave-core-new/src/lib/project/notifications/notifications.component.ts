a39827050606189ae65a22e903395d6c4ae83822
KaroFr <frohnapfelkarolin@gmail.com>
2021-04-23 08:53:17 +0200

    added notification component

---
+++ projects/wave-core-new/src/lib/project/notifications/notifications.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {NotificationService} from '../../notification.service';
+import {Notification } from '../../notification.service';
+
+@Component({
+  selector: 'wave-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NotificationsComponent implements OnInit {
+    notifications: Array<Notification>;
+
+
+    constructor( private notificationService: NotificationService) {
+        this.notifications = [];
+    }
+
+    ngOnInit(): void {
+        this.notifications = this.notificationService.notifications;
+        this.notifications = this.notifications.reverse();
+    }
+
+}
