f61bacb907b11e61074d5787a827659a9ddc6e2a
KaroFr <frohnapfelkarolin@gmail.com>
2021-04-23 09:33:27 +0200

    deleted initialAmount and used min instead

--- projects/wave-core-new/src/lib/operators/dialogs/helpers/multi-layer-selection/multi-layer-selection.component.ts
+++ projects/wave-core-new/src/lib/operators/dialogs/helpers/multi-layer-selection/multi-layer-selection.component.ts
@@ -63,11 +63,6 @@ export class MultiLayerSelectionComponent implements ControlValueAccessor, OnCha
     @Input() max = 1;
 
     /**
-     * The initial amount of elements to select.
-     */
-    @Input() initialAmount = 1;
-
-    /**
      * The type is used as a filter for the layers to choose from.
      */
     @Input() types: Array<ResultType> = ResultTypes.ALL_TYPES;
@@ -93,7 +88,7 @@ export class MultiLayerSelectionComponent implements ControlValueAccessor, OnCha
 
     constructor(private projectService: ProjectService) {
         this.layerSubscription = this.filteredLayers.subscribe((filteredLayers) => {
-            this.selectedLayers.next(this.layersForInitialSelection(filteredLayers, [], this.initialAmount));
+            this.selectedLayers.next(this.layersForInitialSelection(filteredLayers, [], this.min));
         });
 
         this.selectionSubscription = this.selectedLayers.subscribe((selectedLayers) => {
@@ -113,15 +108,10 @@ export class MultiLayerSelectionComponent implements ControlValueAccessor, OnCha
 
     ngOnChanges(changes: {[propertyName: string]: SimpleChange}): void {
         let minMaxInitialChanged = false;
-        let initialChange = false;
 
         // eslint-disable-next-line guard-for-in
         for (const propName in changes) {
             switch (propName) {
-                case 'initialAmount':
-                    initialChange = changes[propName].isFirstChange();
-                    minMaxInitialChanged = true;
-                    break;
                 case 'min':
                 case 'max':
                     minMaxInitialChanged = true;
@@ -175,11 +165,6 @@ export class MultiLayerSelectionComponent implements ControlValueAccessor, OnCha
                         // add selected layers
                         const difference = this.min - amountOfLayers;
                         this.selectedLayers.next(selectedLayers.concat(this.layersForInitialSelection(filteredLayers, [], difference)));
-                    }
-
-                    if (initialChange) {
-                        // set initial layers
-                        this.selectedLayers.next(this.layersForInitialSelection(filteredLayers, [], this.initialAmount));
                     }
                 });
         }
