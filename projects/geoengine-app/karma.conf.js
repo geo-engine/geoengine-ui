6e39f1fffdb6439afbacf29f40d74fa1c3f9b59a
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2021-05-12 17:33:59 +0200

    use `puppeteer` to only have a single test runner for Chrome and Chromium

--- projects/geoengine-app/karma.conf.js
+++ projects/geoengine-app/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+// find Chrome either as Chrome or Chromium
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -25,8 +28,9 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['ChromeHeadless', 'ChromiumHeadless'],
+        browsers: ['ChromeHeadless'],
         singleRun: true,
         restartOnFileChange: true,
+        failOnEmptyTestSuite: false,
     });
 };
