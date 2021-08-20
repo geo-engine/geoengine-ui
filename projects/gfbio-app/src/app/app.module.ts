518c587ed5537be5b20bd92eef86141ca1e10481
Michael Mattig <michael.m@ttig.de>
2021-08-20 15:32:26 +0200

    add gfbio app

---
+++ projects/gfbio-app/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {
+    Config,
+    LayoutService,
+    MapService,
+    NotificationService,
+    ProjectService,
+    RandomColorService,
+    SidenavRef,
+    SpatialReferenceService,
+    TabsService,
+    UserService,
+    WaveCoreModule,
+} from 'wave-core';
+import {AppConfig} from './app-config.service';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserAnimationsModule,
+        BrowserModule,
+        HttpClientModule,
+        RouterModule.forRoot([{path: '**', component: AppComponent}], {useHash: true}),
+        WaveCoreModule,
+    ],
+    providers: [
+        {provide: Config, useClass: AppConfig},
+        {
+            provide: APP_INITIALIZER,
+            useFactory: (config: AppConfig) => (): Promise<void> => config.load(),
+            deps: [Config],
+            multi: true,
+        },
+        LayoutService,
+        MapService,
+        NotificationService,
+        ProjectService,
+        RandomColorService,
+        SidenavRef,
+        SpatialReferenceService,
+        UserService,
+        TabsService,
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
