fb06ebf6ad0640dfb8a6d2a3f55772ab69db06da
Lars Happel <57327151+ClockworkOnion@users.noreply.github.com>
2022-08-12 14:30:27 +0200

    Fixed double equals to triple equals

--- projects/wave-core-new/src/lib/operators/dialogs/column-range-filter/column-range-filter.component.ts
+++ projects/wave-core-new/src/lib/operators/dialogs/column-range-filter/column-range-filter.component.ts
@@ -168,7 +168,7 @@ export class ColumnRangeFilterComponent implements OnInit, OnDestroy {
 
                 if (this.columnTypes.get(value.attribute) !== VectorColumnDataTypes.Text && Number(range.min) > Number(range.max)) {
                     this.appendErrorMsg(`Filter ${index + 1} (${value.attribute}): Minimum must be smaller than maximum!\n`);
-                } else if (range.min > range.max && this.columnTypes.get(value.attribute) == VectorColumnDataTypes.Text) {
+                } else if (range.min > range.max && this.columnTypes.get(value.attribute) === VectorColumnDataTypes.Text) {
                     this.appendErrorMsg(`Filter ${index + 1} (${value.attribute}): Minimum must be alphabetically before maximum!\n`);
                 }
             });
