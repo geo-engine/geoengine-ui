426cf2ee8f30ee0443486e2581c97fe1b701909c
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2022-08-31 15:40:48 +0200

    order public-api lexicographically

--- projects/wave-core-new/src/public-api.ts
+++ projects/wave-core-new/src/public-api.ts
@@ -68,11 +68,11 @@ export * from './lib/operators/dialogs/histogram-operator/histogram-operator.com
 export * from './lib/operators/dialogs/mean-raster-pixel-values-over-time-dialog/mean-raster-pixel-values-over-time-dialog.component';
 export * from './lib/operators/dialogs/operator-list/operator-list.component';
 export * from './lib/operators/dialogs/point-in-polygon-filter/point-in-polygon-filter.component';
+export * from './lib/operators/dialogs/raster-type-conversion/raster-type-conversion.component';
 export * from './lib/operators/dialogs/raster-vector-join/raster-vector-join.component';
 export * from './lib/operators/dialogs/scatterplot-operator/scatterplot-operator.component';
 export * from './lib/operators/dialogs/statistics-plot/statistics-plot.component';
 export * from './lib/operators/dialogs/temporal-raster-aggregation/temporal-raster-aggregation.component';
-export * from './lib/operators/dialogs/raster-type-conversion/raster-type-conversion.component';
 export * from './lib/plots/plot-detail-view/plot-detail-view.component';
 export * from './lib/plots/plot-list-entry/plot-list-entry.component';
 export * from './lib/plots/plot-list/plot-list.component';
