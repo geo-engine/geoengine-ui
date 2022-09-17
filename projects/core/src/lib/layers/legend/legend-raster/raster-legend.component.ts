5e0f367232a389ccfe88e8eca18ef3ede344aa22
Lars Happel <57327151+ClockworkOnion@users.noreply.github.com>
2022-09-17 09:07:08 +0200

    Preparations

--- projects/core/src/lib/layers/legend/legend-raster/raster-legend.component.ts
+++ projects/core/src/lib/layers/legend/legend-raster/raster-legend.component.ts
@@ -87,4 +87,27 @@ export class RasterLegendComponent implements OnInit, OnChanges {
             this.numberPipeParameters = calculateNumberPipeParameters(symbology.colorizer.getBreakpoints());
         }
     }
+
+    roundToTwoDecimals(toRound: number): number {
+        return Math.round(toRound * 100) / 100;
+    }
+
+    shortenDecimals(toShorten: number): number {
+        const integerPart: number = Math.floor(toShorten);
+        const decimals: number = toShorten - integerPart;
+        let zeroes: number = 3; // Third index is where the decimals start. Since we want to round to 3 places after the last 0, this should land us there (?)
+        const decimalsString = decimals.toString();
+        console.log(decimalsString); // TODO delete log
+        while (decimalsString.charAt(zeroes) === '0') {
+            zeroes++;
+        }
+        const roundAt: number = Math.pow(10, zeroes);
+        return Math.round((toShorten * roundAt) / roundAt);
+    }
+
+    /*
+    12.423562343 -> 12.424
+    3.00034525 -> 3.000346
+    3.00010000453 -> ?
+    */
 }
