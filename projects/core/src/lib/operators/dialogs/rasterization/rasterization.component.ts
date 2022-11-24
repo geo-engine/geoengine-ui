174ea970b8b7261de9ee753b171c9231428a29c2
arianegr <test>
2022-11-24 12:44:05 +0100

    updates on addLayer method

--- projects/core/src/lib/operators/dialogs/rasterization/rasterization.component.ts
+++ projects/core/src/lib/operators/dialogs/rasterization/rasterization.component.ts
@@ -1,12 +1,11 @@
 import {Component, ChangeDetectionStrategy, OnDestroy} from '@angular/core';
-import {RandomColorService} from '../../../util/services/random-color.service';
 import {Layer, RasterLayer} from '../../../layers/layer.model';
 import {ResultTypes} from '../../result-type.model';
 import {RasterSymbology} from '../../../layers/symbology/symbology.model';
 import {Validators, FormControl, FormGroup, FormBuilder} from '@angular/forms';
 import {geoengineValidators} from '../../../util/form.validators';
 import {ProjectService} from '../../../project/project.service';
-import {map, mergeMap} from 'rxjs/operators';
+import {mergeMap} from 'rxjs/operators';
 import {WorkflowDict} from '../../../backend/backend.model';
 import {DensityRasterizationDict, GridRasterizationDict, RasterizationDict} from '../../../backend/operator.model';
 import {Subscription} from 'rxjs';
@@ -17,12 +16,16 @@ interface RasterizationForm {
     rasterization: FormGroup<DensityForm> | FormGroup<GridForm>;
 }
 
+// for Tab UI:
+// type RasterizationType = 0 | 1;
 type RasterizationType = 'grid' | 'density';
 
 interface GridOrDensityForm {
     gridOrDensity: FormControl<RasterizationType>;
 }
 interface GridForm extends GridOrDensityForm {
+    // for Tab UI:
+    // gridOrDensity: FormControl<0>;
     gridOrDensity: FormControl<'grid'>;
     gridSizeMode: FormControl<'fixed' | 'relative'>;
     resolution: FormGroup<{
@@ -35,6 +38,8 @@ interface GridForm extends GridOrDensityForm {
     }>;
 }
 interface DensityForm extends GridOrDensityForm {
+    // for Tab UI:
+    // gridOrDensity: FormControl<1>;
     gridOrDensity: FormControl<'density'>;
     radius: FormControl<number>;
     stddev: FormControl<number>;
@@ -54,7 +59,7 @@ export class RasterizationComponent implements OnDestroy {
     readonly form: FormGroup<RasterizationForm>;
     readonly subscriptions: Array<Subscription> = [];
 
-    constructor(private randomColorService: RandomColorService, private projectService: ProjectService, private formBuilder: FormBuilder) {
+    constructor(private projectService: ProjectService, private formBuilder: FormBuilder) {
         const layerControl = new FormControl<Layer | undefined>(undefined, {
             nonNullable: true,
             validators: [Validators.required],
@@ -62,7 +67,7 @@ export class RasterizationComponent implements OnDestroy {
         this.form = new FormGroup<RasterizationForm>({
             name: this.formBuilder.nonNullable.control<string>('Rasterized', [Validators.required, geoengineValidators.notOnlyWhitespace]),
             layer: layerControl,
-            rasterization: this.defaultGrid(),
+            rasterization: this.initialGrid(),
         });
     }
 
@@ -72,10 +77,12 @@ export class RasterizationComponent implements OnDestroy {
         }
     }
 
-    defaultGrid(): FormGroup<GridForm> {
+    initialGrid(): FormGroup<GridForm> {
         const fb = this.formBuilder.nonNullable;
         const gridMode = fb.control<'fixed' | 'relative'>('fixed');
         return fb.group<GridForm>({
+            // for Tab UI:
+            // gridOrDensity: this.createRasterizationType(0),
             gridOrDensity: this.createRasterizationType('grid'),
             gridSizeMode: gridMode,
             resolution: this.formBuilder.group({
@@ -89,122 +96,124 @@ export class RasterizationComponent implements OnDestroy {
         });
     }
 
-    defaultDensity(): FormGroup<DensityForm> {
+    initialDensity(): FormGroup<DensityForm> {
         return this.formBuilder.nonNullable.group<DensityForm>({
+            // for Tab UI:
+            // gridOrDensity: this.createRasterizationType(1),
             gridOrDensity: this.createRasterizationType('density'),
             radius: this.formBuilder.nonNullable.control<number>(10.0, Validators.required),
             stddev: this.formBuilder.nonNullable.control<number>(10.0, Validators.required),
         });
     }
 
+    add(): void {
+        const pointsLayer = this.form.controls['layer'].value as Layer;
+        const layerName: string = this.form.controls['name'].value;
+        const params = this.rasterizationParams();
+
+        if (!params) {
+            return;
+        }
+
+        this.projectService
+            .getAutomaticallyProjectedOperatorsFromLayers([pointsLayer])
+            .pipe(
+                mergeMap(([points]) => {
+                    const workflow: WorkflowDict = {
+                        type: 'Raster',
+                        operator: {
+                            type: 'Rasterization',
+                            params: {
+                                gridOrDensity: params,
+                            },
+                            sources: {
+                                vector: points,
+                            },
+                        } as RasterizationDict,
+                    } as WorkflowDict;
+                    return this.projectService.registerWorkflow(workflow);
+                }),
+                mergeMap((workflowId) =>
+                    this.projectService.addLayer(
+                        new RasterLayer({
+                            workflowId,
+                            name: layerName,
+                            symbology: RasterSymbology.fromRasterSymbologyDict({
+                                type: 'raster',
+                                opacity: 0.8,
+                                colorizer: {
+                                    type: 'linearGradient',
+                                    breakpoints: [
+                                        {value: 0, color: [0, 0, 0, 255]},
+                                        {value: 255, color: [255, 255, 255, 255]},
+                                    ],
+                                    defaultColor: [0, 0, 0, 255],
+                                    noDataColor: [0, 0, 0, 255],
+                                },
+                            }),
+                            isLegendVisible: false,
+                            isVisible: true,
+                        }),
+                    ),
+                ),
+            )
+            .subscribe();
+    }
+
+    // for Tab UI:
+    // protected createRasterizationType<T extends RasterizationType>(selectedIndex: T): FormControl<T> {
     protected createRasterizationType<T extends RasterizationType>(name: T): FormControl<T> {
         const fb = this.formBuilder.nonNullable;
+        // for Tab UI:
+        // const rasterType = fb.control<T>(selectedIndex, Validators.required);
         const rasterType = fb.control<T>(name, Validators.required);
         this.subscriptions.push(rasterType.valueChanges.subscribe((value) => this.changeRasterization(value)));
         return rasterType;
     }
-
+    // for Tab UI:
+    // protected changeRasterization(rasterization: number): void {
     protected changeRasterization(rasterization: string): void {
+        // for Tab UI:
+        // if (rasterization === 0) {
+        //     this.form.setControl('rasterization', this.initialGrid());
+        // } else if (rasterization === 1) {
+        //     this.form.setControl('rasterization', this.initialDensity());
+        // }
         if (rasterization === 'grid') {
-            this.form.setControl('rasterization', this.defaultGrid());
+            this.form.setControl('rasterization', this.initialGrid());
         } else if (rasterization === 'density') {
-            this.form.setControl('rasterization', this.defaultDensity());
+            this.form.setControl('rasterization', this.initialDensity());
         }
     }
 
-    add(): void {
-        const pointsLayer = this.form.controls['layer'].value as Layer;
-
-        const name: string = this.form.controls['name'].value;
-        //TODO
+    private rasterizationParams(): GridRasterizationDict | DensityRasterizationDict | null {
         const rasterization = this.form.controls.rasterization.value;
-        let radiuss = 0;
-        let stddevs = 0;
-        if (rasterization.gridOrDensity === 'density') {
-            radiuss = rasterization.radius ? rasterization.radius : 0;
-            stddevs = rasterization.stddev ? rasterization.stddev : 0;
+        let params: GridRasterizationDict | DensityRasterizationDict | null = null;
+        if (rasterization.gridOrDensity === 'grid') {
+            // for Tab UI:
+            // if (rasterization.gridOrDensity === 0) {
+            return (params = {
+                type: 'grid',
+                spatialResolution: {
+                    x: rasterization?.resolution?.resX ?? 10,
+                    y: rasterization?.resolution?.resY ?? 10,
+                },
+                gridSizeMode: rasterization?.gridSizeMode ?? 'fixed',
+                originCoordinate: {
+                    x: rasterization?.origin?.originX ?? 0,
+                    y: rasterization?.origin?.originY ?? 0,
+                },
+            });
         }
-
-        //TODO
-        const sourceOperators = this.projectService.getAutomaticallyProjectedOperatorsFromLayers([pointsLayer]);
-
-        sourceOperators
-            .pipe(
-                map(([points]) => {
-                    var workflow;
-                    if (rasterization.gridOrDensity === 'grid') {
-                        workflow = {
-                            type: 'Raster',
-                            operator: {
-                                type: 'Rasterization',
-                                params: {
-                                    gridOrDensity: {
-                                        type: 'grid',
-                                        spatialResolution: {
-                                            x: rasterization?.resolution?.resX,
-                                            y: rasterization?.resolution?.resY,
-                                        },
-                                        gridSizeMode: rasterization.gridSizeMode,
-                                        originCoordinate: {
-                                            x: rasterization?.origin?.originX,
-                                            y: rasterization?.origin?.originY,
-                                        },
-                                    } as GridRasterizationDict,
-                                },
-                                sources: {
-                                    vector: points,
-                                },
-                            } as RasterizationDict,
-                        } as WorkflowDict;
-                    } else {
-                        workflow = {
-                            type: 'Raster',
-                            operator: {
-                                type: 'Rasterization',
-                                params: {
-                                    gridOrDensity: {
-                                        type: 'density',
-                                        radius: radiuss,
-                                        stddev: stddevs,
-                                    } as DensityRasterizationDict,
-                                },
-                                sources: {
-                                    vector: points,
-                                },
-                            } as RasterizationDict,
-                        } as WorkflowDict;
-                    }
-
-                    this.projectService
-                        .registerWorkflow(workflow)
-                        .pipe(
-                            mergeMap((workflowId) =>
-                                this.projectService.addLayer(
-                                    new RasterLayer({
-                                        workflowId,
-                                        name: name,
-                                        symbology: RasterSymbology.fromRasterSymbologyDict({
-                                            type: 'raster',
-                                            opacity: 0.8,
-                                            colorizer: {
-                                                type: 'linearGradient',
-                                                breakpoints: [
-                                                    {value: 0, color: [0, 0, 0, 255]},
-                                                    {value: 255, color: [255, 255, 255, 255]},
-                                                ],
-                                                defaultColor: [0, 0, 0, 255],
-                                                noDataColor: [0, 0, 0, 255],
-                                            },
-                                        }),
-                                        isLegendVisible: false,
-                                        isVisible: true,
-                                    }),
-                                ),
-                            ),
-                        )
-                        .subscribe();
-                }),
-            )
-            .subscribe();
+        // for Tab UI:
+        // if (rasterization.gridOrDensity === 1) {
+        if (rasterization.gridOrDensity === 'density') {
+            return (params = {
+                type: 'density',
+                radius: rasterization?.radius ?? 10,
+                stddev: rasterization?.stddev ?? 10,
+            });
+        }
+        return params;
     }
 }
