5485a6de35bca4aae38796f1194c2f0869f22316
Michael Mattig <michael.m@ttig.de>
2023-01-10 14:55:12 +0100

    replace SessionStream with TokenForRequest

--- projects/core/src/lib/layer-collections/layer-collection.service.ts
+++ projects/core/src/lib/layer-collections/layer-collection.service.ts
@@ -46,26 +46,26 @@ export class LayerCollectionService {
 
     getLayerCollectionItems(provider: UUID, collection: string, offset = 0, limit = 20): Observable<LayerCollectionDict> {
         return this.userService
-            .getSessionStream()
-            .pipe(mergeMap((session) => this.backend.getLayerCollectionItems(session.sessionToken, provider, collection, offset, limit)));
+            .getSessionTokenForRequest()
+            .pipe(mergeMap((session) => this.backend.getLayerCollectionItems(session, provider, collection, offset, limit)));
     }
 
     getRootLayerCollectionItems(offset = 0, limit = 20): Observable<LayerCollectionDict> {
         return this.userService
-            .getSessionStream()
-            .pipe(mergeMap((session) => this.backend.getRootLayerCollectionItems(session.sessionToken, offset, limit)));
+            .getSessionTokenForRequest()
+            .pipe(mergeMap((session) => this.backend.getRootLayerCollectionItems(session, offset, limit)));
     }
 
     getLayer(provider: UUID, layer: string): Observable<LayerDict> {
         return this.userService
-            .getSessionStream()
-            .pipe(mergeMap((session) => this.backend.getLayerCollectionLayer(session.sessionToken, provider, layer)));
+            .getSessionTokenForRequest()
+            .pipe(mergeMap((session) => this.backend.getLayerCollectionLayer(session, provider, layer)));
     }
 
     registerAndGetLayerWorkflowId(providerId: UUID, layerId: string): Observable<UUID> {
         return this.userService
-            .getSessionStream()
-            .pipe(mergeMap((session) => this.backend.registerWorkflowForLayer(session.sessionToken, providerId, layerId)));
+            .getSessionTokenForRequest()
+            .pipe(mergeMap((session) => this.backend.registerWorkflowForLayer(session, providerId, layerId)));
     }
 
     getWorkflowIdMetadata(workflowId: UUID): Observable<VectorLayerMetadata | RasterLayerMetadata> {
@@ -74,8 +74,8 @@ export class LayerCollectionService {
 
     getWorkflowIdMetadataDict(workflowId: UUID): Observable<ResultDescriptorDict> {
         return this.userService
-            .getSessionStream()
-            .pipe(mergeMap((session) => this.backend.getWorkflowMetadata(workflowId, session.sessionToken)));
+            .getSessionTokenForRequest()
+            .pipe(mergeMap((session) => this.backend.getWorkflowMetadata(workflowId, session)));
     }
 
     /**
