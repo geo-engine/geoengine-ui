d87cd9bf5e4f0495bca9ffd980e808bc26c032f1
Johannes Drönner <droenner@mathematik.uni-marburg.de>
2023-05-31 17:05:07 +0200

    lint

--- projects/gis/src/app/guards/can-register.guard.ts
+++ projects/gis/src/app/guards/can-register.guard.ts
@@ -1,5 +1,5 @@
 import {Inject, Injectable} from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 import {AppConfig} from '../app-config.service';
 import {Config} from '@geoengine/core';
@@ -7,7 +7,7 @@ import {Config} from '@geoengine/core';
 @Injectable({
     providedIn: 'root',
 })
-export class CanRegisterGuard  {
+export class CanRegisterGuard {
     constructor(@Inject(Config) private readonly config: AppConfig) {}
 
     canActivate(
