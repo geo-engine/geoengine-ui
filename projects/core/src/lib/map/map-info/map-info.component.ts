2ac20ad03521e0f6d9bbd9c750a252005a324b2e
Dominik Brandenstein <dominik.brandenstein@uni-marburg.de>
2024-03-21 10:50:13 +0100

    Reorder and align info, switch accuracy

--- projects/core/src/lib/map/map-info/map-info.component.ts
+++ projects/core/src/lib/map/map-info/map-info.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 import {ProjectService} from '../../project/project.service';
 import {MapService} from '../map.service';
-import {combineLatestWith, Observable} from 'rxjs';
+import {BehaviorSubject, combineLatestWith, Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {SpatialReferenceService} from '../../spatial-references/spatial-reference.service';
 
@@ -18,22 +18,42 @@ export class MapInfoComponent {
     @Input()
     public bottom!: number;
 
+    highPrecision: BehaviorSubject<boolean> = new BehaviorSubject(false);
+    private static readonly lowNumFractions = 2;
+    private static readonly highNumFractions = 12;
+
     constructor(
         private mapService: MapService,
         private spatialReferenceService: SpatialReferenceService,
         private projectService: ProjectService,
     ) {}
 
-    info(): Observable<string> {
+    togglePrecision(): void {
+        this.highPrecision.next(!this.highPrecision.getValue());
+    }
+
+    info(): Observable<string[]> {
         return this.projectService.getSpatialReferenceStream().pipe(
             map((srs) => this.spatialReferenceService.getOlProjection(srs).getUnits()),
             combineLatestWith(this.mapService.getViewportSizeStream()),
-            map(
-                ([unit, viewport]) =>
-                    `Resolution: ${viewport.resolution} ${unit}/px
-                     x: (${viewport.extent[0]}, ${viewport.extent[2]})
-                     y: (${viewport.extent[1]}, ${viewport.extent[3]})`,
-            ),
+            map(([unit, viewport]) => {
+                const numFractions = this.highPrecision.getValue() ? MapInfoComponent.highNumFractions : MapInfoComponent.lowNumFractions;
+
+                const resolution = viewport.resolution.toFixed(numFractions);
+                const xMin = this.prependPlusSign(viewport.extent[0].toFixed(numFractions));
+                const xMax = this.prependPlusSign(viewport.extent[2].toFixed(numFractions));
+                const yMin = this.prependPlusSign(viewport.extent[1].toFixed(numFractions));
+                const yMax = this.prependPlusSign(viewport.extent[3].toFixed(numFractions));
+
+                return [resolution, unit, xMin, yMin, xMax, yMax];
+            }),
         );
+    }
+
+    private prependPlusSign(number: string): string {
+        if (number.charAt(0) === '-') {
+            return number;
+        }
+        return '+' + number;
     }
 }
