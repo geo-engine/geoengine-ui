aadee43b15845387a6a69d092aa21eb9cbd9e3e7
Michael Mattig <michael.m@ttig.de>
2024-11-12 21:59:45 +0100

    new dashboard for esg indicator service

---
+++ projects/dashboards/esg-indicator-service/src/app/legend/legend.component.ts
@@ -0,0 +1,17 @@
+import {Component, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges, ChangeDetectorRef, input, signal} from '@angular/core';
+import {RasterLayer} from '@geoengine/common';
+import {CoreModule} from '@geoengine/core';
+
+@Component({
+    selector: 'geoengine-legend', // eslint-disable-line @angular-eslint/component-selector
+    templateUrl: './legend.component.html',
+    styleUrls: ['./legend.component.scss'],
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CoreModule],
+})
+export class LegendComponent {
+    layer = input<RasterLayer>();
+
+    visible = signal(true);
+}
