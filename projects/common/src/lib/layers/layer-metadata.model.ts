39891dacfa35aa3da1eefba4898c8a4f2f98ea26
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-01-29 17:33:23 +0100

    update dependencies

--- projects/common/src/lib/layers/layer-metadata.model.ts
+++ projects/common/src/lib/layers/layer-metadata.model.ts
@@ -67,8 +67,8 @@ export class VectorLayerMetadata extends LayerMetadata {
         super(spatialReference, time, bbox);
 
         this.dataType = dataType;
-        this.dataTypes = Immutable.Map(dataTypes);
-        this.measurements = Immutable.Map(measurements);
+        this.dataTypes = Immutable.Map(Object.entries(dataTypes));
+        this.measurements = Immutable.Map(Object.entries(measurements));
     }
 
     static override fromDict(dict: VectorResultDescriptorDict): VectorLayerMetadata {
