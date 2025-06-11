2a04244073f873f449ac3465dd50603e46a44340
Mika Jeske <Jeskem@students.uni-marburg.de>
2025-06-11 16:04:08 +0200

    fix(manager): Show Unit/Classes in result descriptor

--- projects/manager/src/app/result-descriptors/vector-result-descriptor/vector-result-descriptor.component.ts
+++ projects/manager/src/app/result-descriptors/vector-result-descriptor/vector-result-descriptor.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input} from '@angular/core';
-import {Measurement, VectorColumnInfo, TypedVectorResultDescriptor} from '@geoengine/openapi-client';
+import {Component, Input, OnChanges} from '@angular/core';
+import {Measurement, TypedVectorResultDescriptor, VectorColumnInfo} from '@geoengine/openapi-client';
 
 interface Column {
     name: string;
@@ -13,7 +13,7 @@ interface Column {
     styleUrl: './vector-result-descriptor.component.scss',
     standalone: false,
 })
-export class VectorResultDescriptorComponent {
+export class VectorResultDescriptorComponent implements OnChanges {
     @Input() resultDescriptor!: TypedVectorResultDescriptor;
 
     displayedColumns: string[] = ['name', 'dataType', 'measurement'];
@@ -23,7 +23,13 @@ export class VectorResultDescriptorComponent {
         return date.toISOString();
     }
 
-    get columnsDataSource(): Column[] {
+    columns: Column[] = [];
+
+    ngOnChanges() {
+        this.columns = this.columnsDataSource();
+    }
+
+    private columnsDataSource(): Column[] {
         const keys = Object.keys(this.resultDescriptor.columns).sort();
 
         const columns: Column[] = [];
