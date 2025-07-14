ece84f7caa0298d0417c5e62e4e1a877598a8b71
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-07-14 11:14:03 +0200

    remove unused import

--- projects/core/src/lib/operators/dialogs/helpers/operator-dialog-container/operator-dialog-container.component.ts
+++ projects/core/src/lib/operators/dialogs/helpers/operator-dialog-container/operator-dialog-container.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, input, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 
