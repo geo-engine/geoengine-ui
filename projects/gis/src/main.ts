72c488fbd8bb325eced86a7395bf1d5d8e549793
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2025-07-28 08:16:13 +0200

    remove app modules and make components entrypoint

--- projects/gis/src/main.ts
+++ projects/gis/src/main.ts
@@ -1,7 +1,51 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {AppConfig} from './app/app-config.service';
+import {
+    CoreConfig,
+    LayoutService,
+    MapService,
+    ProjectService,
+    SidenavRef,
+    SpatialReferenceService,
+    TabsService,
+    CoreModule,
+} from '@geoengine/core';
+import {CommonConfig, NotificationService, RandomColorService, UserService} from '@geoengine/common';
+import {provideAppInitializer, inject, importProvidersFrom} from '@angular/core';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
+import {AppRoutingModule} from './app/app-routing.module';
+import {AppComponent} from './app/app.component';
 
-import {AppModule} from './app/app.module';
-
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, CoreModule),
+        AppConfig,
+        {
+            provide: CoreConfig,
+            useExisting: AppConfig,
+        },
+        {
+            provide: CommonConfig,
+            useExisting: AppConfig,
+        },
+        provideAppInitializer(() => {
+            const initializerFn = (
+                (config: AppConfig) => (): Promise<void> =>
+                    config.load()
+            )(inject(AppConfig));
+            return initializerFn();
+        }),
+        LayoutService,
+        MapService,
+        NotificationService,
+        ProjectService,
+        RandomColorService,
+        SidenavRef,
+        SpatialReferenceService,
+        UserService,
+        TabsService,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideAnimations(),
+    ],
+}).catch((err) => console.error(err));
