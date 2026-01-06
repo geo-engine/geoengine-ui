df3d743ecfdad2bebc667c81473e8c3a615dd492
Christian Beilschmidt <christian.beilschmidt@geoengine.de>
2026-01-06 10:26:08 +0100

    refactor layer list menu

--- projects/core/src/lib/layers/layer-list/layer-list-menu/layer-list-menu.component.ts
+++ projects/core/src/lib/layers/layer-list/layer-list-menu/layer-list-menu.component.ts
@@ -2,20 +2,19 @@ import {Component, ChangeDetectionStrategy, inject} from '@angular/core';
 import {LayoutService} from '../../../layout.service';
 import {MapService} from '../../../map/map.service';
 import {ProjectService} from '../../../project/project.service';
-import {Observable} from 'rxjs';
 import {CoreConfig} from '../../../config.service';
 import {MatIconButton} from '@angular/material/button';
 import {MatTooltip} from '@angular/material/tooltip';
 import {MatMenuTrigger, MatMenu, MatMenuItem} from '@angular/material/menu';
 import {MatIcon} from '@angular/material/icon';
-import {NgSwitch, NgSwitchCase, NgIf, AsyncPipe} from '@angular/common';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'geoengine-layer-list-menu',
     templateUrl: './layer-list-menu.component.html',
     styleUrls: ['./layer-list-menu.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [MatIconButton, MatTooltip, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem, NgSwitch, NgSwitchCase, NgIf, AsyncPipe],
+    imports: [MatIconButton, MatTooltip, MatMenuTrigger, MatIcon, MatMenu, MatMenuItem],
 })
 export class LayerListMenuComponent {
     layoutService = inject(LayoutService);
@@ -26,16 +25,14 @@ export class LayerListMenuComponent {
     /**
      * sends if the layerlist should be visible
      */
-    readonly layerListVisibility$: Observable<boolean>;
+    readonly layerListIsVisibile = toSignal(this.layoutService.getLayerListVisibilityStream(), {
+        initialValue: true,
+    });
 
     /**
      * sends if the map should be a grid (or else a single map)
      */
-    readonly mapIsGrid$: Observable<boolean>;
-
-    constructor() {
-        this.layerListVisibility$ = this.layoutService.getLayerListVisibilityStream();
-
-        this.mapIsGrid$ = this.mapService.isGrid$;
-    }
+    readonly mapIsGrid = toSignal(this.mapService.isGrid$, {
+        initialValue: false,
+    });
 }
